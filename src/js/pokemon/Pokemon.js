5cc39fe3e169947b97e0567b4f39e1ca899ed397
Gavyn Partlow <gavyn.partlow@gmail.com>
2019-03-22 13:23:13 -0400

    Max out Overall Score when Selecting IVs and Level
    This new algorithm maximizes the "overall" score, which is just
    the effective hp, atk, and sta stats of the Pokemon multiplied
    together and divided by 1000.

--- src/js/pokemon/Pokemon.js
+++ src/js/pokemon/Pokemon.js
@@ -31,13 +31,13 @@ function Pokemon(id, i, b){
 	this.fastMovePool = [];
 	this.chargedMovePool = [];
 	this.legacyMoves = [];
-	
+
 	this.typeEffectiveness = getTypeEffectivenessArray(b);
-	
+
 	this.fastMove = null;
 	this.chargedMoves = [];
 	this.chargedMoves = [];
-	
+
 	this.isCustom = false; // Does this Pokemon have custom set levels and IV's?
 	
 	this.index = i;
@@ -110,18 +110,67 @@ function Pokemon(id, i, b){
 			// Scale Pokemon to selected CP
 			// If the Pokemon can't reach the CP limit without IV's, increment until it reaches the CP limit or 15/15/15
 			
-			var targetCPM = 1;
-			var iv = -1;
+			var atkIV = 15;
+			var defIV = 15;
+			var hpIV = 15;
+			var level = 40;
+			var calcCP = 0;
+			var minLevelCP = 0;
+			var maxLevelCP = 0;
+			var overall = 0;
+            var cpms = [0.094,0.135137432,0.16639787,0.192650919,0.21573247,0.236572661,0.25572005,0.273530381,0.29024988,0.306057377,0.3210876,0.335445036,0.34921268,0.362457751,0.37523559,0.387592406,0.39956728,0.411193551,0.42250001,0.432926419,0.44310755,0.453059958,0.46279839,0.472336083,0.48168495,0.4908558,0.49985844,0.508701765,0.51739395,0.525942511,0.53435433,0.542635767,0.55079269,0.558830576,0.56675452,0.574569153,0.58227891,0.589887917,0.59740001,0.604818814,0.61215729,0.619399365,0.62656713,0.633644533,0.64065295,0.647576426,0.65443563,0.661214806,0.667934,0.674577537,0.68116492,0.687680648,0.69414365,0.700538673,0.70688421,0.713164996,0.71939909,0.725571552,0.7317,0.734741009,0.73776948,0.740785574,0.74378943,0.746781211,0.74976104,0.752729087,0.75568551,0.758630378,0.76156384,0.764486065,0.76739717,0.770297266,0.7731865,0.776064962,0.77893275,0.781790055,0.78463697,0.787473578,0.79030001];
+            var cpm = 0;
+            var iv_options = [];
+            var level_options = [];
+            var overall_options = [];
 
-			while((iv < 15) && (targetCPM > .7903)){
-				iv++;
-
-				targetCPM = Math.sqrt( (targetCP * 10) / ((this.baseStats.atk+iv) * Math.pow(this.baseStats.def+iv, 0.5) * Math.pow(this.baseStats.hp+iv, 0.5)));
+            while (level >= 1) {
+            	minLevelCP = Math.floor((this.baseStats.atk) * Math.pow(this.baseStats.def, 0.5) * Math.pow(this.baseStats.hp, 0.5) * Math.pow(cpms[(level - 1) * 2], 2) / 10);
+            	maxLevelCP = Math.floor((this.baseStats.atk + 15) * Math.pow(this.baseStats.def + 15, 0.5) * Math.pow(this.baseStats.hp + 15, 0.5) * Math.pow(cpms[(level - 1) * 2], 2) / 10);
+            	if (maxLevelCP < targetCP || minLevelCP > targetCP) {
+            		level -= 0.5;
+            		continue;
+				}
+				cpm = cpms[(level - 1) * 2];
+            	hpIV = 15;
+            	while (hpIV >= 0) {
+            		defIV = 15;
+            		while (defIV >= 0) {
+            			atkIV = 15;
+            			while (atkIV >= 0) {
+            				calcCP = Math.floor(((this.baseStats.atk + atkIV) * Math.pow(this.baseStats.def + defIV, 0.5) * Math.pow(this.baseStats.hp + hpIV, 0.5) * Math.pow(cpms[(level - 1) * 2], 2)) / 10);
+            				if (calcCP <= targetCP) {
+            					var atk = cpm * (this.baseStats.atk + atkIV);
+            					var def = cpm * (this.baseStats.def + defIV);
+            					var hp = Math.floor(cpm * (this.baseStats.hp + hpIV));
+                                overall = Math.round((hp * atk * def) / 1000);
+                                iv_options.push(atkIV, defIV, hpIV);
+                                level_options.push(level);
+                                overall_options.push(overall);
+                            }
+                            atkIV--;
+						}
+						defIV--;
+					}
+					hpIV--;
+				}
+				level -= 0.5;
 			}
-			
-			this.ivs.atk = this.ivs.def = this.ivs.hp = iv;
 
-			this.cpm = Math.min(targetCPM, .7903);
+			if (overall_options.length > 0) {
+            	var index = overall_options.indexOf(Math.max.apply(Math, overall_options));
+            	this.ivs.atk = iv_options[index * 3];
+            	this.ivs.def = iv_options[index * 3 + 1];
+            	this.ivs.hp = iv_options[index * 3 + 2];
+            	this.level = level_options[index];
+            	overall_options.sort();
+			} else {
+            	this.ivs.atk = 15;
+            	this.ivs.def = 15;
+            	this.ivs.hp = 15;
+            	this.level = 40;
+			}
+			this.cpm = cpms[(this.level - 1) * 2];
 		}
 
 		//Set effective stats
