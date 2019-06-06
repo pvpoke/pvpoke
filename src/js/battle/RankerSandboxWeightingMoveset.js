b2cdd6a253a8f04cb7ee128b15d4d430601f38b9
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2019-06-06 09:05:28 -0500

    Removed Ho-Oh from Championships rankings, moveset updates

--- src/js/battle/RankerSandboxWeightingMoveset.js
+++ src/js/battle/RankerSandboxWeightingMoveset.js
@@ -133,7 +133,7 @@ var RankerMaster = (function () {
 				}
 
 				if(cup.name == "championships-1"){
-					permaBannedList = permaBannedList.concat(["lugia","cresselia","deoxys","deoxys_attack","deoxys_defense","deoxys_speed","mew","celebi","latios","latias","uxie","mesprit","azelf","melmetal","celebi","zapdos","articuno","moltres","suicune","entei","raikou","regirock","registeel","regice","ho-oh"]);
+					permaBannedList = permaBannedList.concat(["lugia","cresselia","deoxys","deoxys_attack","deoxys_defense","deoxys_speed","mew","celebi","latios","latias","uxie","mesprit","azelf","melmetal","celebi","zapdos","articuno","moltres","suicune","entei","raikou","regirock","registeel","regice","ho_oh"]);
 				}
 
 				// If you want to rank specfic Pokemon, you can enter their species id's here
