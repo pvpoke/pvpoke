5e762ccc636e638f14fdf2c680429283340c8f03
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2019-06-19 14:37:14 -0500

    Reversed cup order, Jungle moveset adjustments
    Cups are now displayed in reverse order with the most recent Cup at the top. Adjusted some Jungle recommended movesets.

--- src/modules/cupselect.php
+++ src/modules/cupselect.php
@@ -1,15 +1,15 @@
 <select class="cup-select">
 	<option value="all">All Pokemon</option>
-	<option value="boulder">Boulder Cup</option>
-	<option value="twilight">Twilight Cup</option>
-	<option value="tempest">Tempest Cup</option>
-	<option value="kingdom">Kingdom Cup</option>
-	<option value="nightmare">Nightmare Cup</option>
-	<option value="regionals-1">Season 1 Regionals</option>
-	<option value="championships-1">Season 1 Championships</option>
-	<option value="rainbow">Rainbow Cup</option>
 	<option value="jungle">Jungle Cup</option>
-
+	<option value="rainbow">Rainbow Cup</option>
+	<option value="championships-1">Season 1 Championships</option>
+	<option value="regionals-1">Season 1 Regionals</option>
+	<option value="nightmare">Nightmare Cup</option>
+	<option value="kingdom">Kingdom Cup</option>
+	<option value="tempest">Tempest Cup</option>
+	<option value="twilight">Twilight Cup</option>
+	<option value="boulder">Boulder Cup</option>
+	
 	<?php if(strpos($_SERVER['REQUEST_URI'], 'battle') !== false): ?>
 		<option value="custom">Custom</option>
 	<?php endif; ?>
