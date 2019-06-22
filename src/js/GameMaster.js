df7437aa4d3703dfdbb0eedb0d1b3a3de5aa9a56
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2019-06-21 21:22:26 -0500

    Updated Tangrowth Jungle moveset
    Overrode Tangrowth's moveset to include Sludge Bomb instead of Solar Beam.

--- src/js/GameMaster.js
+++ src/js/GameMaster.js
@@ -11,7 +11,7 @@ var GameMaster = (function () {
 		object.groups = [];
 		object.loadedData = 0;
 
-		$.getJSON( webRoot+"data/gamemaster.json?v=110", function( data ){
+		$.getJSON( webRoot+"data/gamemaster.json?v=111", function( data ){
 			object.data = data;
 
 			// Sort Pokemon alphabetically for searching
@@ -89,7 +89,7 @@ var GameMaster = (function () {
 			var key = cup + "" + category + "" + league;
 
 			if(! object.rankings[key]){
-				var file = webRoot+"data/"+cup+"/"+category+"/"+"rankings-"+league+".json?v=110";
+				var file = webRoot+"data/"+cup+"/"+category+"/"+"rankings-"+league+".json?v=111";
 
 				$.getJSON( file, function( data ){
 					object.rankings[key] = data;
@@ -109,7 +109,7 @@ var GameMaster = (function () {
 			var key = group;
 
 			if(! object.groups[key]){
-				var file = webRoot+"data/groups/"+group+".json?v=110";
+				var file = webRoot+"data/groups/"+group+".json?v=111";
 
 				$.getJSON( file, function( data ){
 
