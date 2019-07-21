cd43a2e6e370634c977fbe9a611f70c9b0e2c7b3
Vandré Israel Brunazo <vibrunazo@gmail.com>
2019-07-21 12:19:10 -0300

    remove cups not used by pvpoke

--- src/modules/cupselect.php
+++ src/modules/cupselect.php
@@ -9,11 +9,6 @@
 	<option value="tempest">Tempest Cup</option>
 	<option value="twilight">Twilight Cup</option>
 	<option value="boulder">Boulder Cup</option>
-	<option value="kanto">Kanto Cup</option>
-	<option value="johto">Johto Cup</option>
-	<option value="hoenn">Hoenn Cup</option>
-	<option value="uc">Ultra Counter</option>
-	<option value="liguinha3">Liguinha 3</option>
 	
 	<?php if(strpos($_SERVER['REQUEST_URI'], 'battle') !== false): ?>
 		<option value="custom">Custom</option>
