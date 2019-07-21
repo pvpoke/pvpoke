3144bf33cb78b1e65ecf506f9676cd95d146115a
Vandré Israel Brunazo <vibrunazo@gmail.com>
2019-07-21 11:58:32 -0300

    config for production

--- src/modules/config.php
+++ src/modules/config.php
@@ -1,6 +1,6 @@
 <?php
-$WEB_ROOT = '/vpvp/src/';
-// $WEB_ROOT = '/';
+// $WEB_ROOT = '/vpvp/src/';
+$WEB_ROOT = '/';
 $WEB_HOST = 'http://' . $_SERVER['HTTP_HOST'] . $WEB_ROOT;
 $UA = '';
 $GOOGLE_AD_CLIENT = '';
