469b61941c8815d2906d3fbe38b23d38aa049e94
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2019-09-07 19:04:09 -0500

    Cliffhanger article and assets

---
+++ src/articles/cliffhanger-team-building.php
@@ -0,0 +1,73 @@
+<?php
+
+
+$META_TITLE = 'Team Building for GO Stadium Cliffhanger';
+
+$META_DESCRIPTION = 'GO Stadium has introduced an exciting new format called Cliffhanger! Learn how to spend your points and build your team.';
+
+require_once '../header.php';
+
+?>
+
+<div class="section article white">
+	<h1>Team Building for GO Stadium Cliffhanger</h1>
+	<div class="date">Last updated September 7th, 2019</div>
+	<p>The GO Stadium Discord, one of the largest communities for PvP discussion and remote tournaments, has announced an exciting new way to play: Cliffhanger! Cliffhanger is a format where you have to build your team with a limited number of points. Let’s dive into the format and how to build your Cliffhanger team!</p>
+	<p>Also be sure to check out the full rules in the <a href="https://reddit.com/r/TheSilphArena/comments/cvv0q1/go_stadium_announces_cliffhanger_a_4tier_format/ey6kqr7/" target="_blank">announcement post</a>!</p>
+<p>Jump to a section:
+  <ol>
+				<li><a href="#rules">Cliffhanger Rules &amp; Team Archetypes</a></li>
+				<li><a href="#slots">Team-Building with Slots</a></li>
+				<li><a href="#testing">Testing Your Team</a></li>
+	  </ol>
+	<a name="rules"></a>
+<h3 class="article-header"><strong>Cliffhanger Rules &amp; Team Archetypes</strong></h3>
+	<p><img src="<?php echo $WEB_ROOT; ?>assets/articles/cliffhanger-rules.jpg" /></p>
+<p>You have 17 points to build to your team! The  graphic above shows which Pokemon belong to which tiers. The tiers are a team  building challenge, so how can you rise to meet it? </p>
+<p>There are several different team archetypes you  can choose from. Some will allow you to pick that powerful T0 Pokemon while  others give you broader choices from the lower tiers. </p>
+<ul type="disc">
+  <li>9 5 1 1 1 0</li>
+  <li>5 5 5 1 1 0</li>
+  <li>9 1 1 1 1 1</li>
+  <li>5 5 1 1 1 1</li>
+  <li>0 0 0 0 0 0 (if you&rsquo;re crazy)</li>
+</ul>
+
+<p><a name="slots"></a>
+</p>
+<h3 class="article-header"><strong>Team-Building with Slots</strong></h3>
+<p>So you have a team archetype picked out or maybe  you&rsquo;re not even sure yet! Where do you begin with choosing the actual Pokemon  on your team? One way to approach it is to think about your team in terms of  slots. Your team has 6 slots and you need each one to fill a certain kind of  role or coverage. </p>
+<p>The infographic below highlights a few of the  possible picks in different slots and the roles they play for your team: </p>
+	<p><img src="<?php echo $WEB_ROOT; ?>assets/articles/cliffhanger-infographic.jpg" /></p>
+<p>What&rsquo;s the best way to start? Pick a Pokemon you  really like, think about which Pokemon it loses to, and then pick a Pokemon  that covers its weaknesses. Skarmory is countered by Steel-type tanks, for  example, so Whiscash makes a great partner. Toxicroak evaporates against  Confusion users, so it would appreciate a buddy like Sableye or Umbreon. </p>
+<p>Here&rsquo;s a team building example that walks  through each pick: </p>
+<ol start="1" type="1">
+  <li><strong>Altaria (9 points) </strong>has strong       general matchups but loses to Steel and Fairy types.<strong></strong></li>
+  <li><strong>Toxicroak (1 point)</strong> can beat       down Steel types and threaten Fairy types with Sludge Bomb, but loses to       Flying, Psychic, and Ghost types.<strong></strong></li>
+  <li><strong>Magneton (1 point) </strong>with       Discharge and Magnet Bomb counters Flying types, resists Psychic attacks,       and hits Fairy types hard. It loses to Fighting and Ground attacks.<strong></strong></li>
+  <li><strong>Venusaur (5 points) </strong>tears       through Ground and Water-type Pokemon and shores up Fighting and Fairy       weaknesses thanks to its Poison typing. It&rsquo;s most vulnerable to Flying and       Fire types, and Psychic types with Confusion.<strong></strong></li>
+  <li><strong>Lapras (1 point)</strong> with Ice       Shard, Surf, and Skull Bash provides additional Flying and Fire coverage       and supports the team with high natural bulk. It&rsquo;s most vulnerable to       Fighting and Electric types.<strong></strong></li>
+  <li><strong>Alolan Marowak (0 points) </strong>rounds       out the team as a hard counter to Fighting and Psychic types, coverage       with Bone Club, and a resistance to Fairy and Grass-type attacks.</li>
+</ol>
+<p>This gives you a fairly well-rounded team, but  even the example above has some gaps. Sableye poses a problem so Alolan Marowak  could be swapped for Togekiss to similarly counter Fighting types, but this  makes the team more vulnerable to Electric attacks (Raichu with Brick Break  could do some damage). Team building is a game of give and take. See which  picks work for you and experiment with different ways to build out your  coverage! </p>
+<a name="testing"></a>
+  <h3 class="article-header"><strong>Testing Your Team</strong></h3>
+ 	<p>Once you have a team picked, find some friends  to battle and put them to the test! Pay close attention and try to answer the  following questions: </p>
+    <ul type="disc">
+      <li>Which Pokemon do opponents tend to pick against my       team?</li>
+      <li>Which Pokemon do I lack an answer for?</li>
+      <li>Which team compositions are giving me the most trouble?</li>
+      <li>Which team compositions are working best for me?</li>
+      <li>If I could add one Pokemon to my team, which would help       me the most?</li>
+    </ul>
+    <p>Your team might go through several iterations as  you figure out which Pokemon work best for you. Don&rsquo;t be afraid to change it up  and try new things! </p>
+    <p>If you don&rsquo;t have anyone to practice against or  are just looking for additional practice, try your hand against some of the  featured teams in the <a href="<?php echo $WEB_ROOT; ?>train">Training Battles</a>! </p>
+	<div class="share-link-container">
+		<p>Share this article:</p>
+		<div class="share-link">
+			<input type="text" value="https://pvpoke.com/articles/cliffhanger-team-building/" readonly>
+			<div class="copy">Copy</div>
+		</div>
+	</div>
+</div>
+<?php require_once '../footer.php'; ?>
