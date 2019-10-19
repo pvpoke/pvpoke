d75fbb2e21ba53989b166108bee7d4561e9842cd
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2019-10-19 18:15:50 -0500

    Consistency score for rankings
    Introduced a consistency score to the overall rankings, calculated by recommended moveset.

--- src/js/battle/RankerOverall.js
+++ src/js/battle/RankerOverall.js
@@ -138,6 +138,69 @@ var RankerMaster = (function () {
 
 				for(var i = 0; i < rankings.length; i++){
 
+					var r = rankings[i];
+					var pokemon = new Pokemon(r.speciesId, 0, battle);
+					pokemon.initialize();
+
+					// Calculate consistency score based on moveset
+
+					var arr = r.moveStr.split("-");
+					var fastMove = pokemon.fastMovePool[arr[0]];
+					var chargedMoves = [pokemon.chargedMovePool[arr[1]-1]];
+					var consistencyScore = 1;
+
+					// Only calculate with two Charged Moves
+					if((arr.length > 2)&&(arr[2] != "0")){
+						chargedMoves.push(pokemon.chargedMovePool[arr[2]-1]);
+
+						var effectivenessScenarios = [
+							[1, 1]
+							];
+
+						if(chargedMoves[0].type != chargedMoves[1].type){
+							effectivenessScenarios.push(
+								[0.625, 1],
+								[1, 0.625]
+							);
+						}
+
+						// Here we are looking at how depenendent each Pokemon is on baiting in scenarios where both moves are neutral, or one or the other is resisted
+						for(var n = 0; n < effectivenessScenarios.length; n++){
+							// Sort moves by name as a starting point
+							chargedMoves.sort((a,b) => (a.name > b.name) ? -1 : ((b.name > a.name) ? 1 : 0));
+
+							// Need to reset this number because of how movesets are generated
+							chargedMoves[0].dpe = (chargedMoves[0].damage / chargedMoves[0].energy) * effectivenessScenarios[n][0];
+							chargedMoves[1].dpe = (chargedMoves[1].damage / chargedMoves[1].energy) * effectivenessScenarios[n][1];
+							chargedMoves.sort((a,b) => (a.dpe > b.dpe) ? -1 : ((b.dpe > a.dpe) ? 1 : 0));
+
+							// Factor in Power-Up Punch where Pokemon may be consistent spamming it
+
+							if(chargedMoves[1].moveId == "POWER_UP_PUNCH"){
+								chargedMoves[1].dpe *= 2;
+								chargedMoves.sort((a,b) => (a.dpe > b.dpe) ? -1 : ((b.dpe > a.dpe) ? 1 : 0));
+							}
+
+							// Calculate how much fast move vs charged move damage this Pokemon puts out per cycle
+							var cycleFastMoves = Math.ceil(chargedMoves[0].energy / fastMove.energyGain);
+							var cycleFastDamage = fastMove.damage * cycleFastMoves;
+							var cycleDamage = cycleFastDamage + chargedMoves[0].damage;
+
+							var factor = 1;
+							if(chargedMoves[0].energy > chargedMoves[1].energy){
+								factor = (cycleFastDamage / cycleDamage) + ((chargedMoves[0].damage / cycleDamage) * (chargedMoves[1].dpe / chargedMoves[0].dpe));
+							}
+
+							consistencyScore *= factor;
+						}
+
+
+						// Now do a square root mean
+						consistencyScore = Math.pow(consistencyScore, (1/effectivenessScenarios.length));
+					}
+
+					consistencyScore = Math.round(consistencyScore * 1000) / 10;
+
 					/* Let's try a different approach to calculating overall score.
 					* Weigh the top 2 ratings by 9/20 and the bottom 2 ratings by 1/20.
 					* This will better represent specialized Pokemon.
@@ -149,6 +212,8 @@ var RankerMaster = (function () {
 					for(var n = 0; n < rankings[i].scores.length; n++){
 						scores.push(rankings[i].scores[n]);
 					}
+
+					rankings[i].scores.push(consistencyScore);
 
 					var bestScore = Math.max(scores[0], scores[1]);
 					sortedScores.push(
@@ -163,7 +228,7 @@ var RankerMaster = (function () {
 
 					// rankings[i].score = Math.pow( Math.pow(sortedScores[0], 9) * Math.pow(sortedScores[1], 9) * Math.pow(sortedScores[2], 1) * Math.pow(sortedScores[3], 1), (1/20));
 
-					rankings[i].score = Math.pow( Math.pow(bestScore, 12) * Math.pow(sortedScores[0], 6) * Math.pow(sortedScores[1], 1) * Math.pow(sortedScores[2], 1), (1/20));
+					rankings[i].score = Math.pow( Math.pow(bestScore, 12) * Math.pow(sortedScores[0], 6) * Math.pow(sortedScores[1], 1) * Math.pow(sortedScores[2], 1) * Math.pow(consistencyScore, 2), (1/22));
 
 					rankings[i].score = Math.floor(rankings[i].score*10) / 10;
 				}
