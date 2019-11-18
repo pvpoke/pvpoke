2d09b6e821caf5c66b2759a8b4f37788ff077aa5
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2019-11-18 09:40:31 -0600

    Ranking updates for Blast Burn Infernape, Galarian Weezing
    Added Galarian Weezing to the rankings, updated move usage for Blast Burn Infernape.

--- src/js/GameMaster.js
+++ src/js/GameMaster.js
@@ -12,7 +12,7 @@ var GameMaster = (function () {
 		object.teamPools = [];
 		object.loadedData = 0;
 
-		$.getJSON( webRoot+"data/gamemaster.json?v=185", function( data ){
+		$.getJSON( webRoot+"data/gamemaster.json?v=186", function( data ){
 			object.data = data;
 
 			// Sort Pokemon alphabetically for searching
@@ -198,7 +198,7 @@ var GameMaster = (function () {
 			var key = cup + "" + category + "" + league;
 
 			if(! object.rankings[key]){
-				var file = webRoot+"data/"+cup+"/"+category+"/"+"rankings-"+league+".json?v=185";
+				var file = webRoot+"data/"+cup+"/"+category+"/"+"rankings-"+league+".json?v=186";
 
 				$.getJSON( file, function( data ){
 					object.rankings[key] = data;
@@ -218,7 +218,7 @@ var GameMaster = (function () {
 			var key = group;
 
 			if(! object.groups[key]){
-				var file = webRoot+"data/groups/"+group+".json?v=185";
+				var file = webRoot+"data/groups/"+group+".json?v=186";
 
 				$.getJSON( file, function( data ){
 
@@ -241,7 +241,7 @@ var GameMaster = (function () {
 			var key = league + "" + cup;
 
 			if(! object.teamPools[key]){
-				var file = webRoot+"data/training/teams/"+cup+"/"+league+".json?v=185";
+				var file = webRoot+"data/training/teams/"+cup+"/"+league+".json?v=186";
 
 				$.getJSON( file, function( data ){
 					object.teamPools[key] = data;
@@ -286,7 +286,7 @@ var GameMaster = (function () {
 			}
 
 			var bannedList = ["mewtwo","mewtwo_armored","giratina_altered","groudon","kyogre","rayquaza","palkia","dialga","heatran","giratina_origin","darkrai","cobalion"];
-			var permaBannedList = ["rotom","rotom_fan","rotom_frost","rotom_heat","rotom_mow","rotom_wash","phione","manaphy","shaymin_land","shaymin_sky","arceus","arceus_bug","arceus_dark","arceus_dragon","arceus_electric","arceus_fairy","arceus_fighting","arceus_fire","arceus_flying","arceus_ghost","arceus_grass","arceus_ground","arceus_ice","arceus_poison","arceus_psychic","arceus_rock","arceus_steel","arceus_water","kecleon","weezing_galarian","zigzagoon_galarian","linoone_galarian"]; // Don't rank these Pokemon at all yet
+			var permaBannedList = ["rotom","rotom_fan","rotom_frost","rotom_heat","rotom_mow","rotom_wash","phione","manaphy","shaymin_land","shaymin_sky","arceus","arceus_bug","arceus_dark","arceus_dragon","arceus_electric","arceus_fairy","arceus_fighting","arceus_fire","arceus_flying","arceus_ghost","arceus_grass","arceus_ground","arceus_ice","arceus_poison","arceus_psychic","arceus_rock","arceus_steel","arceus_water","kecleon","zigzagoon_galarian","linoone_galarian"]; // Don't rank these Pokemon at all yet
 
 			var maxDexNumber = 493;
 			var releasedGen5 = ["snivy","servine","serperior","tepig","pignite","emboar","oshawott","dewott","samurott","lillipup","herdier","stoutland","purrloin","liepard","pidove","tranquill","unfezant","blitzle","zebstrika","foongus","amoonguss","drilbur","excadrill","litwick","lampent","chandelure","golett","golurk","deino","zweilous","hydreigon","pansage","panpour","pansear","simisage","simipour","simisear","ferroseed","ferrothorn","heatmor","durant","patrat","watchog","klink","klang","klinklang","yamask","cofagrigus","cobalion","meltan","melmetal"];
