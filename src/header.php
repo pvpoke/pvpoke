1d57b5b6d55b526dbbe87eea3eec366f5d499044
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2019-11-18 14:51:12 -0600

    Timeless rankings and group additions
    Added Combusken to the rankings and Ludicolo to the custom meta group.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.12.7.10';
+$SITE_VERSION = '1.12.7.11';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
