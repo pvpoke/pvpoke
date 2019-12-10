c051e0d400f5237ca47cb75d3349b83ac09072fc
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2019-12-10 16:32:28 -0600

    Added Beartic, Cryogonal, and Virizion
    Updated rankings for soon to be released Pokemon.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.12.15';
+$SITE_VERSION = '1.12.15.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
