8ae5e6f3a1a54d57e9dced3643fe01613e7048e7
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2019-12-17 17:17:24 -0600

    Fusion cup ranking updates and training battles
    Updated Fusion Cup ranked movesets and implement Fusion Cup training battles.

--- src/train/index.php
+++ src/train/index.php
@@ -36,6 +36,7 @@ require_once '../header.php';
 				<option value="1500 all">Great League</option>
 				<option value="2500 all">Ultra League</option>
 				<option value="10000 all">Master League</option>
+				<option value="1500 fusion">Fusion Cup</option>
 				<option value="1500 timeless">Timeless Cup</option>
 			</select>
 			<h3 class="section-title">Difficulty</h3>
