6bfa38490a89e2687964f7a63d2f645756a64594
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-01-09 20:58:53 -0600

    Training Team meta analysis
    Incorporated a new meta analysis tool that involves the AI picking against itself and displaying usage rates for each Pokemon. This can help identify which direction a meta will progress.

---
+++ src/js/training/TrainingAnalytics.js
@@ -0,0 +1,182 @@
+/*
+* Interface functionality for move list and explorer
+*/
+
+var InterfaceMaster = (function () {
+    var instance;
+
+    function createInstance() {
+
+
+        var object = new interfaceObject();
+
+		function interfaceObject(matchHandler){
+
+			// It's matchHandlers all the way down
+
+			var self = this;
+			var gm = GameMaster.getInstance();
+			var mode = "tournament"; // Single or tournament
+			var teamSelectMethod = "random"; // Random or manual
+			var partySize = 6;
+			var battle;
+			var roundNumber = 0;
+			var pokemonData = []; // Collection of pick and usage rates for all Pokemon
+
+			this.init = function(){
+				var data = gm.data;
+				battle = new Battle();
+
+				// Load ranking data for movesets
+				gm.loadRankingData(self, "overall", 1500, "all");
+
+				// Event listeners
+
+				$(".league-cup-select").on("change", selectLeague);
+				$(".battle-btn").on("click", startBattle);
+				$("a.random").on("click", randomizeTeam);
+			};
+
+			// Callback after ranking data is loaded
+
+			this.displayRankingData = function(data){
+				console.log("Ranking data loaded");
+			}
+
+			// Hide setup after initiating battle
+
+			this.close = function(){
+				for(var i = 0; i < 4; i++){
+					$(".section").eq(i).slideUp(500);
+				}
+
+				$(".section.updates").slideUp(500);
+
+				$("#main h1").slideUp(500);
+			}
+
+			// Callback for importing a randomly generated roster
+
+			this.runPickProcess = function(roster){
+				var playerA = new Player(0, 3, battle);
+				var playerB = new Player(1, 3, battle);
+
+				for(var i = 0; i < 100; i++){
+					playerA.getAI().generateRoster(partySize, self.collectRosterData);
+					playerB.getAI().generateRoster(partySize, self.collectRosterData);
+
+					playerA.getAI().generateTeam(playerB.getRoster());
+					playerB.getAI().generateTeam(playerA.getRoster());
+
+					self.collectTeamData(playerA.getTeam());
+					self.collectTeamData(playerB.getTeam());
+				}
+
+				pokemonData.sort((a,b) => (a.pickCount > b.pickCount) ? -1 : ((b.pickCount > a.pickCount) ? 1 : 0));
+
+				// Display pick data in table
+
+				var $table = $("<table><tr><td>Pokemon</td><td>Roster Count</td><td>Pick Count</td></tr></table>");
+
+				for(var i = 0; i < pokemonData.length; i++){
+					$table.append("<tr><td>"+pokemonData[i].speciesId+"</td><td>"+pokemonData[i].rosterCount+"</td><td>"+pokemonData[i].pickCount+"</td></tr>");
+				}
+
+				$("#main").append($table);
+			}
+
+			// Push team of 6 data into the current collection
+
+			this.collectRosterData = function(roster){
+				// Search data for Pokemon ID
+
+				for(var i = 0; i < roster.length; i++){
+					var found = false;
+
+					for(var n = 0; n < pokemonData.length; n++){
+						if(roster[i].speciesId == pokemonData[n].speciesId){
+							found = true;
+							pokemonData[n].rosterCount++;
+							continue;
+						}
+					}
+
+					if(! found){
+						pokemonData.push({
+							speciesId: roster[i].speciesId,
+							rosterCount: 3,
+							pickCount: 0
+						});
+					}
+				}
+			}
+
+			// Push team of 3 data into the current collection
+
+			this.collectTeamData = function(team){
+				// Search data for Pokemon ID
+
+				for(var i = 0; i < team.length; i++){
+					for(var n = 0; n < pokemonData.length; n++){
+						if(team[i].speciesId == pokemonData[n].speciesId){
+							found = true;
+							pokemonData[n].pickCount++;
+							continue;
+						}
+					}
+				}
+			}
+
+			// Dispatch battle start to the MatchHandler with provided options
+
+			function startBattle(e){
+				pokemonData = [];
+
+				// Initiate a player to load the training team data
+				var player = new Player(0, 3, battle);
+				player.getAI().generateRoster(partySize, self.runPickProcess);
+			}
+
+			// Event handler for changing the league select
+
+			function selectLeague(e){
+				var vals = $(".league-cup-select option:selected").val().split(" ");
+				var cp = vals[0];
+				var cup = vals[1];
+
+				battle.setCP(cp);
+				battle.setCup(cup);
+			}
+
+			// Give the player a random team
+
+			function randomizeTeam(e){
+				e.preventDefault();
+
+				var difficulty = $(".difficulty-select option:selected").val();
+				var player = new Player(0, difficulty, battle);
+				player.getAI().generateRoster(partySize, self.importRandomizedRoster);
+			}
+
+			// Turn checkboxes on and off
+
+			function checkBox(e){
+				$(this).toggleClass("on");
+			}
+
+		}
+
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+var interface = InterfaceMaster.getInstance();
---
+++ src/train/analytics.php
@@ -0,0 +1,64 @@
+<?php
+
+$META_TITLE = 'Training Battle';
+
+$META_DESCRIPTION = 'Select your team and practice battling against an AI.';
+
+
+require_once '../header.php';
+?>
+<h1>Training Battle Analytics</h1>
+<div class="section home white">
+	<p>Gather pick rate data from AI selected teams.</p>
+</div>
+
+<div class="hide">
+	<?php require '../modules/pokeselect.php'; ?>
+</div>
+
+<div class="section poke-select-container train">
+	<div class="poke">
+		<h3>Your Team</h3>
+		<?php require '../modules/pokemultiselect.php'; ?>
+		<a class="random" href="#">Random</a>
+	</div>
+
+	<div class="poke ai-options">
+		<h3>Settings</h3>
+		<div class="poke-stats">
+			<h3 class="section-title">League &amp; Cup</h3>
+			<select class="league-cup-select">
+				<option value="1500 all">Great League</option>
+				<option value="2500 all">Ultra League</option>
+				<option value="10000 all">Master League</option>
+				<option value="1500 fusion">Fusion Cup</option>
+				<option value="1500 timeless">Timeless Cup</option>
+			</select>
+		</div>
+	</div>
+	<div class="clear"></div>
+</div>
+
+<div class="section">
+	<button class="battle-btn button">Simulate</button>
+</div>
+
+<script src="<?php echo $WEB_ROOT; ?>js/GameMaster.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/pokemon/Pokemon.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/pokemon/Player.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/training/TrainingAnalytics.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/training/BattleInterface.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/training/DecisionOption.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/training/TrainingAI.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/PokeSearch.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/PokeSelect.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/PokeMultiSelect.js?=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/ModalWindow.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/TimelineEvent.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/TimelineAction.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/Battle.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/TeamRanker.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/training/MatchHandler.js?v=<?php echo $SITE_VERSION; ?>"></script>
+
+
+<?php require_once '../footer.php'; ?>
