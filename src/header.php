befb73634fc0aee3b628e5617a7273bfc964e400
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2020-01-21 15:59:52 -0600

    Added Cinccino and Darmanitan lines

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.12.24.2';
+$SITE_VERSION = '1.12.24.3';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
