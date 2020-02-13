0e622b4f7662b9d74b0ac7bbf25a600de6df53f2
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-02-12 20:47:49 -0600

    Training URL fix
    Featured team data file URL had a typo.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.12.31.8';
+$SITE_VERSION = '1.12.31.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
