4f413df838e06d166394e51855d381cb4533c76c
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2020-02-21 13:53:51 -0600

    Added training for GBL Master League
    Added training teams for GBL Master League.

--- src/train/index.php
+++ src/train/index.php
@@ -36,6 +36,7 @@ require_once '../header.php';
 				<option value="" selected disabled>Select a league</option>
 				<option value="1500 gobattleleague">GO Battle League (Great)</option>
 				<option value="2500 gobattleleague">GO Battle League (Ultra)</option>
+				<option value="10000 gobattleleague">GO Battle League (Master)</option>
 				<option value="1500 all">Great League</option>
 				<option value="2500 all">Ultra League</option>
 				<option value="10000 all">Master League</option>
