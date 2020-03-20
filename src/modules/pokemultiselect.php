9d624107a607384c3fdece9ad288a462c1fc5ce0
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-03-19 20:50:42 -0500

    Fixed issue with Voyager Cup custom group selection

--- src/modules/pokemultiselect.php
+++ src/modules/pokemultiselect.php
@@ -16,7 +16,7 @@
 				<option value="great" type="great" class="multi-battle">Great League Meta</option>
 				<option value="ultra" type="ultra" class="hide multi-battle">Ultra League Meta</option>
 				<option value="master" type="master" class="hide multi-battle">Master League Meta</option>
-				<option value="toxic" type="voyager" class="multi-battle">Voyager Cup Meta</option>
+				<option value="voyager" type="voyager" class="multi-battle">Voyager Cup Meta</option>
 				<option value="toxic" type="toxic" class="multi-battle">Toxic Cup Meta</option>
 			</select>
 			<div class="flex quick-fill-buttons">
