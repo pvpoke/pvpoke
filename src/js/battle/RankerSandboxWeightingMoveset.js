d18fa4263bd7b9619b2af7242579860ec2d560e9
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-04-21 22:45:37 -0500

    Updated rankings with new default IV's

--- src/js/battle/RankerSandboxWeightingMoveset.js
+++ src/js/battle/RankerSandboxWeightingMoveset.js
@@ -415,10 +415,6 @@ var RankerMaster = (function () {
 					iterations = 1;
 				}
 
-				if((cup.name == "all")&&(battle.getCP() == 10000)){
-					iterations = 1;
-				}
-
 				if((cup.name == "all")&&(battle.getCP() == 2500)){
 					iterations = 1;
 				}
