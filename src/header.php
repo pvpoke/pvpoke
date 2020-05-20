46fe219eedb7f3d22d6b5349f481ee87534da59b
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-05-20 15:57:29 -0500

    Added Bullet Seed for Shiftry

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.15.0.7';
+$SITE_VERSION = '1.15.0.8';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
