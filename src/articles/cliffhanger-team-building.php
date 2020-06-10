55ff96e18f221607abcce573f1339c939b2c5491
Empoleon_Dynamite <pvpoke.empoleon@gmail.com>
2020-06-10 13:51:49 -0500

    Updated Cliffhanger graphics and article
    Updated Cliffhanger graphics and article for version 3.1 with Galarian Pokemon.

--- src/articles/cliffhanger-team-building.php
+++ src/articles/cliffhanger-team-building.php
@@ -11,9 +11,9 @@ require_once '../header.php';
 
 <div class="section article white">
 	<h1>Team Building for GO Stadium Cliffhanger</h1>
-	<div class="date">Last updated January 4th, 2020</div>
+	<div class="date">Last updated June 10, 2020</div>
 	<p>Cliffhanger is an exciting format where you have to build your team with a limited number of points! It was designed by the <a href="https://discord.gg/fbczGXZ" target="_blank">GO Stadium Discord</a>, one of the largest communities for PvP discussion and remote tournaments, and has been updated with the previous movepool changes and Generation 5 Pokemon. Let’s dive into the format and how to build your Cliffhanger team!</p>
-	<p>Also be sure to check out the full rules in the updated <a href="https://reddit.com/r/TheSilphArena/comments/drdh2j/cliffhanger_tiers_version_21_regi_edition/" target="_blank">announcement post</a>!</p>
+	<p>Also be sure to check out the full rules in the updated <a href="https://reddit.com/r/TheSilphArena/comments/h00kkt/cliffhanger_31_tier_update/" target="_blank">announcement post</a>!</p>
 <p>Jump to a section:
   <ol>
 				<li><a href="#rules">Cliffhanger Rules &amp; Team Archetypes</a></li>
@@ -22,7 +22,7 @@ require_once '../header.php';
 	  </ol>
 	<a name="rules"></a>
 <h3 class="article-header"><strong>Cliffhanger Rules &amp; Team Archetypes</strong></h3>
-	<p><img src="<?php echo $WEB_ROOT; ?>assets/articles/cliffhanger-rules-2-2.jpg" /></p>
+	<p><img src="<?php echo $WEB_ROOT; ?>assets/articles/cliffhanger-rules-3-1.jpg" /></p>
 <p>You have 17 points to build to your team! The  graphic above shows which Pokemon belong to which tiers. The tiers are a team  building challenge, so how can you rise to meet it? </p>
 <p>There are several different team archetypes you  can choose from. Some will allow you to pick that powerful T0 Pokemon while  others give you broader choices from the lower tiers. </p>
 <ul type="disc">
@@ -38,18 +38,18 @@ require_once '../header.php';
 <h3 class="article-header"><strong>Team-Building with Slots</strong></h3>
 <p>So you have a team archetype picked out or maybe  you&rsquo;re not even sure yet! Where do you begin with choosing the actual Pokemon  on your team? One way to approach it is to think about your team in terms of  slots. Your team has 6 slots and you need each one to fill a certain kind of  role or coverage. </p>
 <p>The infographic below highlights a few of the  possible picks in different slots and the roles they play for your team: </p>
-	<p><img src="<?php echo $WEB_ROOT; ?>assets/articles/cliffhanger-infographic-2-2.jpg?v=2" /></p>
+	<p><img src="<?php echo $WEB_ROOT; ?>assets/articles/cliffhanger-infographic-3-1.jpg?v=2" /></p>
 <p>What&rsquo;s the best way to start? Pick a Pokemon you  really like, think about which Pokemon it loses to, and then pick a Pokemon  that covers its weaknesses. Skarmory is countered by Steel-type tanks, for  example, so Whiscash makes a great partner. Toxicroak evaporates against  Confusion users, so it would appreciate a buddy like Sableye or Umbreon. </p>
 <p>Here&rsquo;s a team building example that walks  through each pick: </p>
 <ol start="1" type="1">
   <li><strong>Altaria (9 points) </strong>has strong       general matchups but loses to Steel and Fairy types.<strong></strong></li>
   <li><strong>Toxicroak (1 point)</strong> can beat       down Steel types and threaten Fairy types with Sludge Bomb, but loses to       Flying, Psychic, and Ghost types.<strong></strong></li>
-  <li><strong>Magneton (0 points) </strong>with       Discharge and Magnet Bomb counters Flying types, resists Psychic attacks,       and hits Fairy types hard. It loses to Fighting and Ground attacks.<strong></strong></li>
+  <li><strong>Perrserker (1 point) </strong>with       Foul Play and Iron Head counters Flying types, resists Psychic attacks,       and hits Fairy types hard. It loses to Fighting and Ground attacks.<strong></strong></li>
   <li><strong>Venusaur (5 points) </strong>tears       through Ground and Water-type Pokemon and shores up Fighting and Fairy       weaknesses thanks to its Poison typing. It&rsquo;s most vulnerable to Flying and       Fire types, and Psychic types with Confusion.<strong></strong></li>
-  <li><strong>Lapras (1 point)</strong> with Ice       Shard, Surf, and Skull Bash provides additional Flying and Fire coverage       and supports the team with high natural bulk. It&rsquo;s most vulnerable to       Fighting and Electric types.<strong></strong></li>
-  <li><strong>Alolan Marowak (1 point) </strong>rounds out the team as a hard counter to Fighting and Psychic types, coverage       with Bone Club, and a resistance to Fairy and Grass-type attacks.</li>
+  <li><strong>Rainy Castform (0 points)</strong> with Weather Ball and Thunder provides additional Fire coverage and supports the team with decent bulk. It&rsquo;s most vulnerable to Grass and Electric types.<strong></strong></li>
+  <li><strong>Alolan Muk (1 point) </strong>rounds out the team as a hard counter to Psychic types and a resistance Grass-type attacks.</li>
 </ol>
-<p>This gives you a fairly well-rounded team, but  even the example above has some gaps. Sableye poses a problem so Alolan Marowak  could be swapped for Togekiss to similarly counter Fighting types, but this  makes the team more vulnerable to Electric attacks (Raichu with Brick Break  could do some damage). Team building is a game of give and take. See which  picks work for you and experiment with different ways to build out your  coverage! </p>
+<p>This gives you a fairly well-rounded team, but  even the example above has some gaps. Opposting Altaria could pose pose a problem. Team building is a game of give and take. See which  picks work for you and experiment with different ways to build out your  coverage! </p>
 <p>Be sure to take advantage of the <a href="<?php echo $WEB_ROOT; ?>team-builder/">Team Builder</a> tool, which can help you identify coverage problems for your team and find alternatives.</p>
 <a name="testing"></a>
   <h3 class="article-header"><strong>Testing Your Team</strong></h3>
@@ -62,7 +62,6 @@ require_once '../header.php';
       <li>If I could add one Pokemon to my team, which would help       me the most?</li>
     </ul>
     <p>Your team might go through several iterations as  you figure out which Pokemon work best for you. Don&rsquo;t be afraid to change it up  and try new things! </p>
-    <p>If you don&rsquo;t have anyone to practice against or  are just looking for additional practice, try your hand against some of the  featured teams in the <a href="<?php echo $WEB_ROOT; ?>train">Training Battles</a>! </p>
 	<div class="share-link-container">
 		<p>Share this article:</p>
 		<div class="share-link">
