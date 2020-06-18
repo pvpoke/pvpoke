26e110d42e81c8d5b42329377dcafd13c6a8e288
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-06-18 00:13:03 -0500

    Updated Continentals and Premier Rankings
    Removed Glalie and Galarian Linoon from Continentals, adjusted Pinsir moveset for Premier.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.15.6.5';
+$SITE_VERSION = '1.15.6.6';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
