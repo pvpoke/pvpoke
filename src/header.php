ec0e0f9f0dc021cc13c44402bf0080d338080989
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-07-02 15:06:37 -0500

    Added Swanna
    Added Swanna to rankings.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.15.9.3';
+$SITE_VERSION = '1.15.9.4';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
