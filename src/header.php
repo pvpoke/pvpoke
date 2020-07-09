345b8650064b3ee838ecb67d813f9c90c1b1fb85
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-07-09 06:07:19 -0500

    Added and ranked new Shadow Pokemon

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.15.11.9';
+$SITE_VERSION = '1.15.11.10';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
