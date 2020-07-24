185b22f37b5e096aff885a8220d5fc969d98139c
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-07-24 16:57:18 -0500

    Added Rotom-W to rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.15.12.1';
+$SITE_VERSION = '1.15.12.2';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
