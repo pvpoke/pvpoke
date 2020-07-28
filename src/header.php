b98c8998f4ab49cc5e08fe778d8f2858db694a0d
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-07-28 16:02:51 -0500

    Version update

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.15.13';
+$SITE_VERSION = '1.15.13.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
