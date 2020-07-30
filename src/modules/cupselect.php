824b69324d2da5cd6f0005c45cb63d4ec60e17c2
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-07-29 23:06:35 -0500

    Updated rankings, GO team Up rankings
    Updated movesets for Tentacruel and Mawile.

--- src/modules/cupselect.php
+++ src/modules/cupselect.php
@@ -2,6 +2,7 @@
 	<option value="all" cup="all">All Pokemon</option>
 	<option value="official" cup="premier">Premier Cup</option>
 	<option value="tsa-2" cup="catacomb">Silph Catacomb Cup</option>
+	<option value="community" cup="goteamup">GO Stadium GOTeamUp</option>
 
 	<?php if(strpos($_SERVER['REQUEST_URI'], 'team-builder') !== false): ?>
 		<option value="community" cup="cliffhanger">GO Stadium Cliffhanger</option>
