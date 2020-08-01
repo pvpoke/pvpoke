83769edb206a6176d1febdaa40fcdd22f2c5e768
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-08-01 03:02:57 -0500

    Removed Catacomb Cup

--- src/train/index.php
+++ src/train/index.php
@@ -42,7 +42,6 @@ require_once '../header.php';
 				<option value="1500 all">Great League</option>
 				<option value="2500 all">Ultra League</option>
 				<option value="10000 all">Master League</option>
-				<option value="1500 catacomb">Silph Catacomb Cup</option>
 			</select>
 			<h3 class="section-title">Difficulty</h3>
 			<select class="difficulty-select">
