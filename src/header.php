ab20c17010385c2d4f94d5a231a46f649a2eb0ef
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-09-11 20:36:39 -0500

    Updated Duet Cup rankings
    Removed Sealeo and Walrein.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.16.1.4';
+$SITE_VERSION = '1.16.1.5';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
