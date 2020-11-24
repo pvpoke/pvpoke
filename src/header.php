a49dbebb1178d533ae832d11df8d7643ce0fbbc6
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-11-23 21:00:04 -0600

    Nightfall Cup training added

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.18.10.3';
+$SITE_VERSION = '1.18.10.4';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
