b26176ba568cc4c0289810418b401b66c804dc9f
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-12-16 16:31:00 -0600

    Added Galarian Mr. Mime and Mr. Rime

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.2.3';
+$SITE_VERSION = '1.20.2.4';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
