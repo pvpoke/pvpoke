f863682b1f4dc0192f966664973c857b80259f55
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-12-21 16:07:01 -0600

    Added Techno Blast

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.3.4';
+$SITE_VERSION = '1.20.3.5';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
