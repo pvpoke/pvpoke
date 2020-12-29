de2dfe469ef0be917a79eb8be32d165520622646
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2020-12-29 17:36:58 -0600

    Fixed Holiday team json

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.5.6.1';
+$SITE_VERSION = '1.20.5.7';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
