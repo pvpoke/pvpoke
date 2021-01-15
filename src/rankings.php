644fb700f6dcf2f5d1cc05c0665bb6cf3d760732
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-01-14 18:19:36 -0600

    Updated Great League training analysis

--- src/rankings.php
+++ src/rankings.php
@@ -42,8 +42,8 @@ switch($cup){
 		$league = 'Master League Classic';
 		break;
 
-	case "holiday":
-		$league = 'Holiday Cup';
+	case "kanto":
+		$league = 'Kanto Cup';
 		break;
 
 	case "cerberus":
@@ -104,7 +104,7 @@ require_once 'header.php';
 
 	<p class="description link hide"><b>Tournament Info:</b> <a href="#" target="_blank"></a></p>
 
-	<p>You can enter your GBL encounters <a href="https://docs.google.com/forms/d/e/1FAIpQLScc_gCvXLTUs5tHORiPpEmuF6yL6UJ0j0P03oSNDRQvNNHa0w/viewform" target="_blank">here</a> or at <a href="https://gobattlelog.com/" target="_blank">gobattlelog.com</a> to help provide usage data for ranking updates.</p>
+	<p>Help provide usage data for the rankings <a href="https://docs.google.com/forms/d/e/1FAIpQLScc_gCvXLTUs5tHORiPpEmuF6yL6UJ0j0P03oSNDRQvNNHa0w/viewform" target="_blank">here</a> or at <a href="https://gobattlelog.com/" target="_blank">gobattlelog.com</a>.</p>
 
 	<p>Click or tap the rankings below for more details.</p>
 
