a324508120aca78d7d2c587286f0599c6b1bd1d0
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-01-15 12:16:55 -0600

    Updated Machamp with Payback in Master League

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.7.8';
+$SITE_VERSION = '1.20.7.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
