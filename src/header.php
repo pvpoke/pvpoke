542814cf6c53e8ad9f285fa3117c614780b64f59
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-01-19 18:48:17 -0600

    Added Payback for Shadow Machamp

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.7.14';
+$SITE_VERSION = '1.20.7.15';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
