8c4adb7a13d4925161bafdd5e83b320c69b640a6
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-01-26 16:05:29 -0600

    Fixed Lickitung in custom group

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.8.2';
+$SITE_VERSION = '1.20.8.3';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
