a574f3632cdaddc0934bb625f8ea1a389f66482f
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-02-15 14:47:52 -0600

    Added Labyrinth Cup training data

--- src/train/analysis.php
+++ src/train/analysis.php
@@ -70,7 +70,7 @@ require_once '../header.php';
 		<option value="10000" cup="all">Master League</option>
 		<option value="2500" cup="premier">Premier Cup (Ultra)</option>
 		<option value="10000" cup="premier">Premier Cup (Master)</option>
-		<option value="1500" cup="love">Love Cup</option>
+		<option value="1500" cup="labyrinth">Silph Labyrinth Cup</option>
 	</select>
 
 	<div class="date-updated">Last updated</div>
