193b169e8ab79c71ea23b5be2248446668c69965
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-02-21 15:45:54 -0600

    Kanto Cup team analysis, updated footer

--- src/train/analysis.php
+++ src/train/analysis.php
@@ -70,6 +70,7 @@ require_once '../header.php';
 		<option value="10000" cup="all">Master League</option>
 		<option value="2500" cup="premier">Premier Cup (Ultra)</option>
 		<option value="10000" cup="premier">Premier Cup (Master)</option>
+		<option value="1500" cup="kanto">Kanto Cup</option>
 		<option value="1500" cup="labyrinth">Silph Labyrinth Cup</option>
 	</select>
 
