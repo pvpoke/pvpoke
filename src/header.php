a803595387599c7ed57f22ad5848430db4e23e3d
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-03-28 14:51:37 -0500

    Updated Great League training analysis data

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.14.8';
+$SITE_VERSION = '1.20.14.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
