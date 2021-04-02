b8af60d7b993f316ddd78fafd5250a9572898eae
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-04-02 11:30:02 -0500

    Added Shadow Aipom, Ambipom

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.15';
+$SITE_VERSION = '1.20.15.4';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
