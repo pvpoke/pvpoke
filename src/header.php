c36bb6550aa137a9560a5706a32a075a05174b1a
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-04-12 07:20:04 -0500

    Added Leaf Storm stats

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.16';
+$SITE_VERSION = '1.20.16.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
