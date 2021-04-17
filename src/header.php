af3dbb80fe425aaa7ee0cd0e7325462e58129e28
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-04-17 08:06:42 -0500

    Updated Great League and Remix training analysis

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.20.16.8';
+$SITE_VERSION = '1.20.16.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
