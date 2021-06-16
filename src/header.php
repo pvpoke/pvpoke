40a7a16621a7532a90c35f69e95ff3d9ca4bb28b
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-06-16 13:21:09 -0500

    Updated Ultra and Ultra Premier training analysis

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.22.9.6';
+$SITE_VERSION = '1.22.9.7';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
