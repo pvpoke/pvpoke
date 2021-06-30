dd9d42da7f1d9faae9b4216a6e7c5165265f7c42
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-06-30 12:38:16 -0500

    Added Blast Burn to Emboar

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.22.12.11';
+$SITE_VERSION = '1.22.12.12';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
