c6171a1c06383ee132de9b6e0fe06fd29372eda1
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-07-31 10:53:17 -0500

    Updated Ultra and Ultra Remix training analysis

--- src/train/analysis.php
+++ src/train/analysis.php
@@ -80,6 +80,7 @@ require_once '../header.php';
 		<option value="1500" cup="all" level-cap="50">Great League</option>
 		<option value="1500" cup="remix" level-cap="50">Great League (Remix)</option>
 		<option value="2500" cup="all" level-cap="50">Ultra League</option>
+		<option value="2500" cup="remix" level-cap="50">Ultra League (Remix)</option>
 		<option value="2500" cup="premier" level-cap="50">Ultra League (Premier)</option>
 		<option value="10000" cup="classic" level-cap="40">Master League (Classic)</option>
 	</select>
