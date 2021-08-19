8d28ed440bc34a16d2b0aae195512dcd53991867
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-08-19 18:59:34 -0500

    Added legendary tags for Zacian and Zamazenta
    Removed them from Ultra Premier

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.22.20.1';
+$SITE_VERSION = '1.22.20.2';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
