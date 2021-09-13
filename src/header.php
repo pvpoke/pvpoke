5bf629c0cf8d05bda675ba37b166b04b1e7c4309
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-09-13 15:21:46 -0500

    Added Furfrou, Hydro Cannon Samurott

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.23.1.13';
+$SITE_VERSION = '1.23.1.14';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
--- src/modules/formatselect.php
+++ src/modules/formatselect.php
@@ -1,6 +1,5 @@
 <select class="format-select">
 	<option value="1500" cup="all" meta-group="great">Great League</option>
-	<option value="1500" cup="remix" meta-group="great">Great League (Remix)</option>
 	<option value="2500" cup="all" meta-group="ultra">Ultra League</option>
 	<option value="2500" cup="remix" meta-group="great">Ultra League (Remix)</option>
 	<option value="2500" cup="premierclassic" meta-group="ultrapremierclassic">Ultra League (Premier Classic)</option>
@@ -11,7 +10,6 @@
 	<option value="500" cup="littlejungle" meta-group="littlejungle">Little Jungle Cup</option>
 	<option value="1500" cup="factions" meta-group="factions">Silph Factions (Dungeon)</option>
 	<option value="1500" cup="nightfall" meta-group="nightfall">Silph Factions (Nightfall)</option>
-	<option value="1500" cup="worlds" meta-group="great">Silph Worlds</option>
 
 	<?php if(strpos($_SERVER['REQUEST_URI'], 'team-builder') !== false): ?>
 		<option value="1500" cup="cliffhanger" meta-group="great">GO Stadium Cliffhanger</option>
