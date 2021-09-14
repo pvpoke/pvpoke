be44224f34d3937435e8399a17d2ab0f94244abc
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-09-14 14:13:51 -0500

    Fixed Ultra Remix to use Ultra Remix meta group
    Was previously referencing Great League.

--- src/modules/formatselect.php
+++ src/modules/formatselect.php
@@ -1,7 +1,7 @@
 <select class="format-select">
 	<option value="1500" cup="all" meta-group="great">Great League</option>
 	<option value="2500" cup="all" meta-group="ultra">Ultra League</option>
-	<option value="2500" cup="remix" meta-group="great">Ultra League (Remix)</option>
+	<option value="2500" cup="remix" meta-group="remixultra">Ultra League (Remix)</option>
 	<option value="2500" cup="premierclassic" meta-group="ultrapremierclassic">Ultra League (Premier Classic)</option>
 	<option value="10000" cup="all" meta-group="master">Master League</option>
 	<option value="10000" cup="classic" meta-group="master">Master League (Classic)</option>
