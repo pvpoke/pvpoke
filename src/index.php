d26acb723200c64d6ef28373c5777b603bd0534e
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-09-26 10:28:42 -0500

    Additional update notes

--- src/index.php
+++ src/index.php
@@ -36,11 +36,14 @@
 
 	<h4>v1.24 (September 26, 2021)</h4>
 	<ul>
-		<li>New "Explore Win Conditions" feature in single Battle simulations:</li>
+		<li>New "Explore Win Conditions" feature in Single Battle simulations:</li>
 		<ul>
 			<li>Generates results of 1000 simulations with randomized actions, such as moves, shields, and baiting.</li>
 			<li>Explore best case and worst case outcomes, as well as median outcomes to find potential paths to victory or plays to avoid.</li>
+			<li>This feature replaces the battle histogram that was previously used to display Single Battle results involving moves with chance effects.</li>
 		</ul>
+		<li>Battle results in Single Battle simulations are now always displayed from the left Pokemon's perspective.</li>
+		<li>Key losses in the rankings now display that Pokemon's battle rating instead of the opponent's.</li>
 	</ul>
 
 	<h3>Latest Article</h3>
