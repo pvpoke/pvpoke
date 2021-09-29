c118e1ecd2d418c9a4324308d423c6f1c9b80875
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-09-29 16:38:51 -0500

    Fixed Meloetta moveset in Master League

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.24.1.1';
+$SITE_VERSION = '1.24.2.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
