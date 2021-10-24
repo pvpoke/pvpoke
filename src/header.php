01cc0d41f6b54e63d7eac831ecab7bd7016268cc
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2021-10-24 08:28:31 -0500

    Fixed move counts, added missing Pokemon from Brawler

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.24.3.1';
+$SITE_VERSION = '1.24.4';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
