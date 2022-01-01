b9349b07fd7248727645c6fc7b9dd27d4c99914d
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-01-01 08:29:00 -0600

    Updated Master League Classic top performers

--- src/train/analysis.php
+++ src/train/analysis.php
@@ -87,11 +87,9 @@ require_once '../header.php';
 	<select class="format-select">
 		<option value="1500" cup="all" level-cap="50">Great League</option>
 		<option value="2500" cup="all" level-cap="50">Ultra League</option>
-		<option value="2500" cup="remix" level-cap="50">Ultra League (Remix)</option>
 		<option value="2500" cup="premierclassic" level-cap="40">Ultra League (Premier Classic)</option>
 		<option value="10000" cup="premierclassic" level-cap="40">Master League (Premier Classic)</option>
 		<option value="10000" cup="classic" level-cap="40">Master League (Classic)</option>
-		<option value="1500" cup="holiday" level-cap="50">Holiday Cup</option>
 	</select>
 
 	<div class="date-updated">Last updated</div>
