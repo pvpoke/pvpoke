1ced0c3400a046c91c37194a33863416af028b97
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-01-14 10:22:43 -0600

    Added Shock Drive Genesect

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.24.7.11';
+$SITE_VERSION = '1.24.7.12';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
