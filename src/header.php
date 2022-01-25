0e53ccd385ea30588cbce9b2ef4473d496a31e6c
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-01-25 11:15:14 -0600

    Fixed Shadow status on some Pokemon, added Galarian Slows to Love Cup

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.25.2.7';
+$SITE_VERSION = '1.25.2.8';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
