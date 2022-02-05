b6fbea5c9ed2889bfa32504bf502187b1c4ed533
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-02-05 13:57:41 -0600

    Initial CD article layout and styling

---
+++ src/articles/community-day/22-02-hoppip.php
@@ -0,0 +1,76 @@
+<?php
+
+
+$META_TITLE = 'Hoppip Community Day Guide for PvP';
+
+$META_DESCRIPTION = 'Learn about Hoppip\'s upcoming Community Day, how Acrobatics will be relevant for PvP, and which Jumpluff IV\'s to look out for.';
+
+require_once '../../header.php';
+
+?>
+
+<div class="section article white hoppip">
+	<h1>Hoppip Community Day Guide for PvP</h1>
+	<div class="date">Last updated February 5th, 2022</div>
+	<img src="<?php echo $WEB_ROOT;?>articles/article-assets/developing-trainer-battle-ai/banner.jpg" />
+	<p>Jumpluff's time to shine may be a skip and a hop away! Hoppip Community Day is Saturday, February 12th, and Jumpluff will receive a Flying-type Charged Move, Acrobatics. You can find more info about Hoppip Community Day at the official <a href="https://pokemongolive.com/en/post/communityday-feb22-hoppip/" target="_blank">Pokemon GO blog</a>. Below are the leagues and IV spreads to look out for, and answers to frequently asked questions.</p>
+
+	<a name="overview"></a>
+	<h3 class="article-header"><strong>Overview</strong></h3>
+
+	<div class="cd-features">
+		<div class="feature">
+			<h4>Date &amp; Time</h4>
+			<div class="value">Saturday, February 12th<br></div>
+			<div class="detail">11 am - 5 pm local time</div>
+		</div>
+		<div class="feature">
+			<h4>Exclusive Charged Move</h4>
+			<div class="value">Acrobatics</div>
+			<div class="detail">110 power, 60 energy</div>
+		</div>
+		<div class="feature">
+			<h4>Event Bonus</h4>
+			<div class="value">3x Catch Stardust</div>
+		</div>
+	</div>
+
+	<div class="mega-section">
+		<div class="mega-title">
+			<div class="mega-icon"></div>
+			<h4>Mega Evolve one of these Pokemon during Community Day to earn bonus catch candy!</h4>
+		</div>
+		<div class="mega-list">
+			<div class="mega-item" mega="venusaur">
+				<div class="mega-image"></div>
+				<div class="mega-label">Venusaur</div>
+			</div>
+			<div class="mega-item" mega="charizard-y">
+				<div class="mega-image"></div>
+				<div class="mega-label">Charizard Y</div>
+			</div>
+			<div class="mega-item" mega="pidgeot">
+				<div class="mega-image"></div>
+				<div class="mega-label">Pidgeot</div>
+			</div>
+			<div class="mega-item" mega="aerodactyl">
+				<div class="mega-image"></div>
+				<div class="mega-label">Aerodactyl</div>
+			</div>
+			<div class="mega-item" mega="abomasnow">
+				<div class="mega-image"></div>
+				<div class="mega-label">Abomasnow</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="share-link-container">
+		<p>Share this article:</p>
+		<div class="share-link">
+			<input type="text" value="https://pvpoke.com/articles/community-day/22-02-hoppip/" readonly>
+			<div class="copy">Copy</div>
+		</div>
+	</div>
+</div>
+
+<?php require_once '../../footer.php'; ?>
