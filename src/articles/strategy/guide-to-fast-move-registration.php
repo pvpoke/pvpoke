42473435203cc3cc0c666c96eface735e8c0dbdc
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-02-10 11:49:39 -0600

    Updated Fast Move mechanic article
    Removed references to old mechanic updates, and added sections on optimal move timing and sneaking.

--- src/articles/strategy/guide-to-fast-move-registration.php
+++ src/articles/strategy/guide-to-fast-move-registration.php
@@ -14,34 +14,33 @@ require_once '../../header.php';
 <div class="section article white">
 	<h1>Guide to Fast Move Mechanics</h1>
 	<p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/banner.jpg" style="border:none;"/></p>
-	<div class="date">Last updated October 9th, 2019</div>
-	<p><b>Update October 9th, 2019</b> - Since app version 0.157.0, the mechanics for Fast Move registration have changed! This article and the site simulations have been updated to reflect those changes.</p>
-	<p>How exactly does Fast Move damage and energy work? In this article, we&rsquo;ll go over the mysterious mechanics behind Fast Moves and how they impact gameplay. </p>
-    <p>Special thanks to FlarkeFiasco, GarretK19383, and TheMisterValor for bringing these mechanics to my attention and helping to compile and explore  video evidence for all sorts of scenarios! This was an incredibly challenging  puzzle to solve and it couldn&rsquo;t have been done without their help.</p>
+	<div class="date">Last updated February 10th, 2022</div>
+	<p>How exactly does Fast Move damage and energy work? What are sneaks, and how should you time your moves? In this article, we&rsquo;ll go over the mechanics behind Fast Moves and how they impact gameplay. </p>
+    <p>Special thanks to FlarkeFiasco, GarretK19383, and TheMisterValor for bringing these mechanics to my attention and helping to compile and video examples for initial Fast Move mechanic work! This was an incredibly challenging  puzzle to solve and it couldn&rsquo;t have been done without their help.</p>
+	<p>Also check out <a href="https://youtu.be/pAtCo8xg700" target="_blank">Wallower</a>, who has in-depth videos discussing move timing and other concepts discussed in this article.</p>
 	  <p>Jump to a section:
 	  <ol>
 				<li><a href="#turns">Turns</a></li>
-				<li><a href="#duration">Fast Move Duration</a></li>
 				<li><a href="#registration">Fast Move Registration</a></li>
+				<li><a href="#optimal-timing">Optimal Move Timing</a></li>
+				<li><a href="#sneaking">Sneaking &amp; Denial</a></li>
 				<li><a href="#sandbox">Sandbox Mode</a></li>
 		  		<li><a href="#differences">Differences in 0.157.0</a></li>
 	  </ol>
 	<a name="turns"></a>
 	<h3 class="article-header">Turns</h3>
 	<p>
-      Trainer Battles are broken up into turns. <strong>Each  turn lasts 0.5 seconds</strong>. On a typical turn, each Pokemon can take one action  - a Fast Move, a Charged Move, or a switch. Let&rsquo;s take a look at how Fast Moves  use turns and interact with one another. </p>
-	<a name="duration"></a>
-		<h3 class="article-header">Fast Move Duration</h3>
+      Trainer Battles are broken up into turns. <strong>Each  turn lasts 0.5 seconds</strong>. On a typical turn, each Pokemon can take one action  - a Fast Move, a Charged Move, or a switch. Let&rsquo;s take a look at how Fast Moves use turns and interact with one another. </p>
 	<p>
       Each Fast Move lasts a certain number of turns,  usually referred to as its &ldquo;duration&rdquo; or &ldquo;cooldown&rdquo;. <strong>Duration determines the  amount of time between your Fast Moves.</strong> The timelines below illustrate  different Fast Moves and their durations. Each circle represents a new action. </p>
 	<p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/mechanics-duration-timeline.png" style="border:none;"/></p>
-	<p>As you can see, a shorter duration allows more  frequent actions. </p>
+	<p>As you can see, a shorter duration allows more  frequent actions. You can fit 2 Counters in each Confusion, or 5 Dragon Breaths in 1 Incinerate.</p>
 	<a name="registration"></a>
 	<h3 class="article-header"> Fast Move Registration</h3>
 	<div class="article-section">
 		<p>
-		  When you use a Fast Move, does it hit right  away? Actually most of the time, no! Since update 0.157.0, <b>Fast Move damage and energy always register on the last turn of the move.</b> This means you will always have the energy from your previous move when you go to perform another move. The earliest time a move can register hasn't changed with this update, but it's now guaranteed to register at that time (previously it required another action happening on the same turn).</p>
-		<p>How does this all work? Check out to the table below for move durations, the number of turns that need to pass before the move hits, and example moves.  Note that 1-turn moves register immediately on the turn they are used. </p>
+		  When you use a Fast Move, it doesn't hit right away. <b>Fast Move damage and energy register on the last turn of the move.</b> If you're using Counter, a 2-turn move, the animation begins on turn 1 and the damage and energy register on turn 2.</p>
+		<p>Check out to the table below for move durations, the number of turns that need to pass before the move hits, and example moves.  Note that 1-turn moves register immediately on the turn they are used. </p>
 		<table cellspacing="0">
 			<tr>
 				<td><b>Duration</b></td>
@@ -66,12 +65,12 @@ require_once '../../header.php';
 			<tr>
 				<td>4</td>
 				<td>3</td>
-				<td>Confusion</td>
+				<td>Confusion, Volt Switch</td>
 			</tr>
 			<tr>
 				<td>5</td>
 				<td>4</td>
-				<td>Volt Switch</td>
+				<td>Incinerate</td>
 			</tr>
 		</table>
 		<p>So what does this look like in action? The  examples below show circles for when you tap to use a Fast Move and squares for  when the move actually hits. </p>
@@ -80,49 +79,54 @@ require_once '../../header.php';
 		  <p>Above we have a mirror match where both Pokemon&rsquo;s  Fast Moves have the same duration. Damage and energy register on the 2nd turn of the move, so play alternates between Fast Moves being used and Fast Moves hitting. </p>
 		  <h3>2-Turn Move vs. 4-Turn Move</h3>
 	<p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/mechanics-register-2-4-new.png" style="border:none;"/></p>
-		  <p>Things play out similarly when the Fast Moves involved  are even with one another. For example, Counter (2 turns) and Confusion (4  turns) happen in step so Confusion registers simultaneously with every other Counter. If you're a Confusion user, you're no longer stuck performing extra Confusions like you were pre-0.157.0.</p>
+		  <p>Things play out similarly when the Fast Moves involved  are even with one another. For example, Counter (2 turns) and Confusion (4  turns) happen in step so Confusion registers simultaneously with every other Counter.</p>
 		<h3>1-Turn Move vs. 3-Turn Move</h3>
 <p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/mechanics-register-1-3.png" style="border:none;"/></p>
-		<p>Battles against Pokemon with 1-turn moves will play exactly like before, since 1-turn moves previously registered their opponent's Fast Moves at the earliest possible time.</p>
+		<p>In battles against a 1-turn move, the 1-turn move registers damage and energy each turn. Pokemon with 1 turn moves can maximize their move timing to throw at any interval in the opponent's animation.</p>
 		  <h3>2-Turn Move vs. 3-Turn Move</h3>
-<p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/assets/articles/mechanics-register-2-3-new.png" style="border:none;"/></p>
-		  Pre-0.157.0, 2-turn vs. 3-turn move interactions were complex and unpredictable. Now both moves register at steady intervals.</p>
+<p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/mechanics-register-2-3-new.png" style="border:none;"/></p>
+		  <p>Some Pokemon matchups involve move timings that are out of sync. For example, a 2 turn and a 3 turn move line up every 3rd move. This can require tricky timing.</p>
+		  <p>Note that <b>Fast Moves take priority over Charged Moves</b>. This means if a Fast Move lands on the same turn that you try to use a Charged Move, your Pokemon can faint and will not fire its Charged Move. 1-turn Fast Moves like Dragon Breath are particularly good at taking advantage of this; for example, if your opponent's Pokemon is left at minimal HP and has a Charged Move stored, you can bring in a Pokemon with Dragon Breath to "snipe" them and prevent the Charged Move from firing.</p>
 	</div>
+
+	<a name="optimal-timing"></a>
+	<h3 class="article-header">Optimal Move Timing</h3>
+	<p>Why does timing matter?</p>
+	<p>When you use a Charged Move, you interrupt any ongoing Fast Moves to begin the Charged Move sequence. The Charged Move sequence resets all Pokemon cooldowns/animations. Afterward, both Pokemon can immediately act again. <b>When you interrupt an opponent's Fast Move, you effectively grant them "free" turns</b> and allow them to attack more frequently than if you had not interrupted them.</p>
+
+	<p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/interruption-example.png" style="border:none;"/></p>
+
+	<p>In the above example, Counter Medicham (2 turns) is throwing Ice Punches at Incinerate Talonflame (5 turns). Incinerate has a very long cooldown, but each time Medicham uses a Charged Move, Talonflame is able to immediately attack again. Ordinarily, it would take 15 turns for Talonflame to use Incinerate 3 times. Here, it takes 8 turns for Talonflame to use Incinerate 3 times because Medicham is interrupting its Fast Moves and granting Talonflame "free" turns.</p>
+
+	<p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/optimized-timing.png" style="border:none;"/></p>
+
+	<p>Here, Medicham is timing its Charged Moves optimally. By throwing Ice Punch on the last turn of Incinerate (the same turn it hits), Medicham grants Talonflame zero "free" turns.</p>
+
+	<p><img src="<?php echo $WEB_ROOT; ?>articles/article-assets/guide-to-fast-move-registration/optimal-timing-guide.png" style="margin: 0 auto;"/></p>
+
+	<p><b>The goal of optimal move timing is to reduce the number of "free" turns you grant your opponent when you use a Charged Move.</b> The graphic above highlights how many free turns you would grant your opponent if you interrupted their Fast Move on the specified turn. It's most optimal to throw your Charged Move <i>on the last turn</i> of your opponent's Fast Move. Throwing at this time results in zero "free" turns and doesn't allow your opponent to attack on the same turn as your Charged Move (see "Sneaking" below).</p>
+
+	<p>Optimal move timing is largely impossible in mirror matches or other matchups where your Fast Moves align (such as a 2 turn vs 4 turn Fast Moves).</p>
+
+	<a name="sneaking"></a>
+	<h3 class="article-header">Sneaking &amp; Denial</h3>
+
+	<p>As mentioned above, a Pokemon gets one action each turn - a Fast Move, a Charged Move, or a switch. When you use a Charged Move, your opponent still gets their action on that turn - oftentimes a Fast Move.</p>
+	<p>The phenomonon called <i>sneaking</i> or <i>leaking</i> is when a Pokemon uses a Fast Move on the same turn that an opponent uses a Charged Move. When this happens, that Pokemon's Fast Move is interrupted on its first turn, effectively granting the entire Fast Move "for free". This timing is the least optimal and can be catastrophic in matchups against longer Fast Moves like Incinerate.</p>
+
+	<p>However, sneaking is currently an inconsistent mechanic. Sometimes when a Pokemon attempts to use a Fast Move on the same turn that an opponent uses a Charged Move, the Fast Move doesn't occur. This is typically referred to as <i>Fast Move denial</i>. <b>Fast Move denial cannot be consistently reproduced or triggered,</b> so it's usually safer to practice optimal move timing rather than rely on fast move denial.</p>
+
 <a name="sandbox"></a>
 		<h3 class="article-header">Sandbox Mode</h3>
 	<p>
-      When you enter Sandbox Mode, circle  icons appear above the timeline to indicate when each players can use an  action, like what&rsquo;s been shown in the article so far. These appear at regular  intervals like you might be used to, dictated by Fast Move duration. <strong>Click  on the circular Tap icons to select an action for that turn.</strong></p>
+      You can use the site's Sandbox Mode to further explore the impact of move timing and mechanics. When you enter Sandbox Mode, circle  icons appear above the timeline to indicate when each players can use an  action, like what&rsquo;s been shown in the article so far. These appear at regular  intervals like you might be used to, dictated by Fast Move duration. <strong>Click  on the circular Tap icons to select an action for that turn.</strong></p>
     <p>You can select one of the following actions: </p>
     <ul type="disc">
       <li><strong>Fast Move - </strong>This player will use a Fast       Move. This action will queue a new Fast Move to be registered as described in this article.<strong></strong></li>
       <li><strong>Charged Move - </strong>This player       will use one of their Charged Moves. Note that a Charged Move will fail if a Fast Move that faints the user also registers on that turn.<strong></strong></li>
       <li><strong>Wait - </strong>This player will take no action on the selected turn.</li>
     </ul>
-	<a name="differences"></a>
-	<h3 class="article-header"> Differences in 0.157.0</h3>
-	<p>A big question is how have matchups changed? Are there any major new wins or losses? Here's a summary of what you can expect to find!</p>
-	<ul>
-		<li>Flipped matchups will be ones that were previously very close with a difference of one or two Fast Moves, or IV's.</li>
-		<li>Damage and energy registration now happen at more predictable intervals. 3-turn moves will behave the same whether they're against a 1-turn move or a 4-turn move.</li>
-		<li>Pokemon are no longer overtapping, but that goes for their opponents as well. This generally benefits most Pokemon equally. Matchups involving a 3-turn move and a 2 or 4-turn move are the most suspectible to change because of how erratic move registration was previously.</li>
-	</ul>
 
-<!--<a name="videos"></a>
-	<h3 class="article-header">Video Examples</h3>
-	<p>
-      Now, it&rsquo;s time to put this all together. Below  are video examples of real-world battles with their corresponding simulations.  Hopefully these help illustrate the mechanics at play and ground the  simulations as you explore them. </p>
-	<p class="center">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/WKcO_b_29tY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	</p>
-	<p>In the example above, Drapion and Xatu both have 3-turn moves (Infestation and Air Slash) so they gain energy and deal damage in step with one another. Mathematically, Drapion should be able to use Crunch after 5 Infestations, but it needs to use a 6th because the energy from its 5th Infestation doesn't register until both Pokemon use a 6th Fast Move.</p>
-	<p class="center">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/sBKWcxWSmws" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	</p>
-	<p>Here we have Hypno with a 4-turn move (Confusion) and Venusaur with a 2-turn move (Vine Whip). Venusaur gets 2 Vine Whips for every Confusion. Because they land evenly with one another, Hypno needs to use 6 Confusions to reach Shadow Ball.</p>
-	<p class="center">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/t9mR8RTdyTI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	</p>
-	<p>This example pulls out all the bells and whistles. Alolan Raichu has a 2-turn move (Spark) against a 3-turn move (Infestation), so energy and damage register in a staggered pattern. While Drapion beats Raichu to the first Charged Move, it also registers Raichu's energy early enough for Raichu to beat it to the next. Because of how these two moves interact, Raichu isn't dependent on registering its own attack.</p>-->
 	<div class="share-link-container">
 		<p>Share this article:</p>
 		<div class="share-link">
