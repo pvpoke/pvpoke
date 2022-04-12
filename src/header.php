ad81e7ed91c384b71a59fcdf099b92922f846cfb
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-04-11 19:58:32 -0500

    Added Tapu Bulu and Shadow Alolan Exeggutor

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.26.4.4';
+$SITE_VERSION = '1.26.4.5';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
