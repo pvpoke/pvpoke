519329ec0be94d69c40a2fcdaaba8a0e0ecd1546
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-04-16 15:46:23 -0500

    Updated contributors page, version update for Pokebox fix

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.27.0';
+$SITE_VERSION = '1.27.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
