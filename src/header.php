ffbdf0026f197d543a0b182ea69519c0c1a2fe16
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-04-22 16:20:59 -0500

    Added Bewear to Firefly Cup

--- src/header.php
+++ src/header.php
@@ -114,7 +114,7 @@ if(! isset($OG_IMAGE)){
 <?php endif; ?>
 
 <?php if(strpos($_SERVER['REQUEST_URI'], 'articles') !== false): ?>
-	<link rel="stylesheet" type="text/css" href="<?php echo $WEB_ROOT; ?>css/article-extras.css?v=3">
+	<link rel="stylesheet" type="text/css" href="<?php echo $WEB_ROOT; ?>css/article-extras.css?v=4">
 <?php endif; ?>
 
 <?php if((isset($_SETTINGS->theme))&&($_SETTINGS->theme != "default")): ?>
