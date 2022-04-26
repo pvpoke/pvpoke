3b9dba1b8b9c0ae4a0ef0d7ee34b65a6f9363bb6
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-04-26 08:10:56 -0500

    Relisted Kanto Cup training

--- src/train/index.php
+++ src/train/index.php
@@ -41,8 +41,7 @@ require_once '../header.php';
 				<option value="10000 classic">GO Battle League (Master Classic)</option>
 				<option value="2500 premierclassic">Ultra Premier Classic</option>
 				<option value="10000 premierclassic">Master Premier Classic</option>
-				<option value="500 littlejungle">Little Jungle Cup</option>
-				<option value="1500 halloween">Halloween Cup</option>
+				<option value="1500 kanto">Kanto Cup</option>
 				<option value="1500 all">Great League</option>
 				<option value="2500 all">Ultra League</option>
 				<option value="10000 all">Master League</option>
