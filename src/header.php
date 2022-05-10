aeb37b62c09939e7915ec087bb3139c6130f4fce
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-05-10 09:53:51 -0500

    Tapu Fini updates and rankings
    Updated Tapu Fini's default IV's to have a level floor of 20. Updated rankings with Tapu Fini.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.28.3';
+$SITE_VERSION = '1.28.3.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
