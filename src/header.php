6623c0c64645710b16d8898ab125c8a1dce1ee87
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-06-06 12:30:13 -0500

    Added Tyrantrum and Aurorus families

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.28.3.22';
+$SITE_VERSION = '1.28.3.23';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
