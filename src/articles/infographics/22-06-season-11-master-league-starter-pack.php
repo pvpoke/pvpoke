3f2609579233f33a434397581ab53a43c9208a25
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-06-14 10:53:19 -0500

    Master League starter pack article content

---
+++ src/articles/infographics/22-06-season-11-master-league-starter-pack.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$META_TITLE = 'Master League Starter Pack for Season 11';
+
+$META_DESCRIPTION = 'Check out the best legendary and non-legendary Pokemon to get started in Master League for GO Battle League Season 11!';
+
+$OG_IMAGE = 'https://pvpoke.com/articles/article-assets/infographics/22-06-season-11-ultra-league-starter-pack/og.jpg';
+
+require_once '../../header.php';
+
+?>
+
+<div class="section article white">
+	<h1>Master League Starter Pack for Season 11</h1>
+	<div class="date">Last updated June 14, 2022</div>
+	<a href="<?php echo $WEB_ROOT;?>articles/article-assets/infographics/22-06-season-11-master-league-starter-pack/master-league-starter-pack-11.jpg" target="_blank">
+		<img src="<?php echo $WEB_ROOT;?>articles/article-assets/infographics/22-06-season-11-master-league-starter-pack/master-league-starter-pack-11.jpg" />
+	</a>
+
+</div>
+
+  <div class="share-link-container">
+		<p>Share this article:</p>
+		<div class="share-link">
+			<input type="text" value="https://pvpoke.com/articles/infographics/22-06-season-11-master-league-starter-pack/" readonly>
+			<div class="copy">Copy</div>
+		</div>
+	</div>
+<?php require_once '../../footer.php'; ?>
