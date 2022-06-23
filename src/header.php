88699ecc2bc33b430e12e03553d38f0f4a00859b
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-06-23 15:06:09 -0500

    Added Brutal Swing to Hydreigon

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.28.5.5';
+$SITE_VERSION = '1.28.5.6';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
@@ -128,7 +128,7 @@ if(! isset($OG_IMAGE)){
 <?php endif; ?>
 
 <?php if(strpos($_SERVER['REQUEST_URI'], 'articles') !== false): ?>
-	<link rel="stylesheet" type="text/css" href="<?php echo $WEB_ROOT; ?>css/article-extras.css?v=5">
+	<link rel="stylesheet" type="text/css" href="<?php echo $WEB_ROOT; ?>css/article-extras.css?v=6">
 <?php endif; ?>
 
 <?php if((isset($_SETTINGS->theme))&&($_SETTINGS->theme != "default")): ?>
