9c18fd82e706786c106105110ca12bae4773f5fd
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-06-25 15:54:45 -0500

    Updated Kanto Cup rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.28.5.8';
+$SITE_VERSION = '1.28.5.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
