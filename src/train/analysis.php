e762eea5e038797c97f24716aeaef728f1291204
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-07-08 16:35:01 -0500

    Updated training analysis
    Updated training analysis data for all leagues.

--- src/train/analysis.php
+++ src/train/analysis.php
@@ -88,7 +88,7 @@ require_once '../header.php';
 	<select class="format-select">
 		<option value="1500" cup="all" level-cap="50">Great League</option>
 		<option value="2500" cup="all" level-cap="50">Ultra League</option>
-		<option value="10000" cup="classic" level-cap="40">Master League (Classic)</option>
+		<option value="10000" cup="all" level-cap="50">Master League</option>
 		<option value="1500" cup="kanto" level-cap="50">Kanto Cup</option>
 	</select>
 
