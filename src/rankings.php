9f8d8c459dfc7044a2276d53e63aba8c14d1d6ec
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-07-26 12:05:35 -0500

    Updated Zapdos moveset

--- src/rankings.php
+++ src/rankings.php
@@ -51,8 +51,8 @@ switch($cup){
 		$league = 'Master League Classic';
 		break;
 
-	case "kanto":
-		$league = 'Kanto Cup';
+	case "hisui":
+		$league = 'Hisui Cup';
 		break;
 
 	case "summer":
@@ -98,7 +98,7 @@ switch($cup){
 	case "catchgofest":
 		$league = 'Catch Cup GO Fest Edition';
 		break;
-		
+
 	case "remix":
 		$league = $league . ' Remix';
 		break;
