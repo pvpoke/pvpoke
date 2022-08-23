b8c7d9e55af3d87dbaf493c519c2f69eccab1b6e
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-08-22 19:10:52 -0500

    Updated training analysis

--- src/train/analysis.php
+++ src/train/analysis.php
@@ -93,7 +93,6 @@ require_once '../header.php';
 		<option value="1500" cup="all" level-cap="50">Great League</option>
 		<option value="2500" cup="all" level-cap="50">Ultra League</option>
 		<option value="10000" cup="all" level-cap="50">Master League</option>
-		<option value="500" cup="element" level-cap="50">Element Cup</option>
 	</select>
 
 	<div class="date-updated">Last updated</div>
