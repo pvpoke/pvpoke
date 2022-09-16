0dfb1d31ac4ffb8568c18062bf11be0e23757817
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-09-16 09:59:28 -0500

    Updated Weather Cup and meta group, Ultra training teams

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.11.9';
+$SITE_VERSION = '1.29.11.10';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
