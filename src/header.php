41cbe2d3b35213af026c71dc7af519d90ffae2fa
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-09-22 21:06:57 -0500

    Updated Toxapex family moveset

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.11.13';
+$SITE_VERSION = '1.29.11.14';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
