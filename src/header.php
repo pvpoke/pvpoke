08962b8ccf98ec7287bf9bb8f2fc9cb8e5c47c7e
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-09-26 16:07:25 -0500

    Added Toxapex family

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.11.16';
+$SITE_VERSION = '1.29.11.17';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
