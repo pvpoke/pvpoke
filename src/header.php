448df59a975655aa8e6d8cd2af502ba2d7687788
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-10-19 12:58:50 -0500

    Set Zacian to Snarl in ML

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.11.30';
+$SITE_VERSION = '1.29.11.31';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
