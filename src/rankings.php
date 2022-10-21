1e1b2c6f07309a6dd47cf62b6a9f3eb45291f358
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-10-21 10:51:02 -0500

    Willpower Cup resources

--- src/rankings.php
+++ src/rankings.php
@@ -51,8 +51,8 @@ switch($cup){
 		$league = 'Master League Classic';
 		break;
 
-	case "evolution":
-		$league = 'Evolution Cup';
+	case "willpower":
+		$league = 'Willpower Cup';
 		break;
 
 	case "littlejungleremix":
