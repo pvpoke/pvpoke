d3a869e1e0624cec9c56471953be6f3912e654f4
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-10-28 19:15:51 -0500

    Added Zorua family, Mega Banette

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.11.35';
+$SITE_VERSION = '1.29.11.36';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
