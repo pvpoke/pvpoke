226525a7d627550d8bcf988830494820fae4ca60
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-11-09 11:32:26 -0600

    Teddiursa CD article initial content

---
+++ src/articles/community-day/22-11-teddiursa.php
@@ -0,0 +1,129 @@
+<?php
+
+
+$META_TITLE = 'Teddirsa Community Day Guide for PvP';
+
+$META_DESCRIPTION = 'Ursulana will be making its debut in Pokemon GO! How does it stand up to the meta and is this bear a necessity for PvP?';
+
+$OG_IMAGE = 'https://pvpoke.com/articles/article-assets/community-day/22-11-teddiursa/og.jpg';
+
+require_once '../../header.php';
+
+?>
+
+<div class="section article white litwick">
+	<h1>Ursaluna Community Day Guide for PvP</h1>
+	<div class="date">Last updated October 12, 2022</div>
+	<img src="<?php echo $WEB_ROOT;?>articles/article-assets/community-day/22-11-teddiursa/banner.jpg" />
+	<p>Teddiursa is moonlighting as a featured Pokemon this Community Day, alongside the debut of Ursaring's new evolution, Ursaluna. This Ground/Normal type can reach a high CP for Master League, but does it have the moves to pull it off? We'll cover Ursaluna's PvP value below as well as tips for evolving. You can find more info on Teddiursa Community Day at the <a href="https://pokemongolive.com/en/post/community-day-november-2022-teddiursa/" target="_blank">Pokemon GO blog</a>. Below are the leagues and IV spreads to look out for, and answers to frequently asked questions.</p>
+
+	<a name="overview"></a>
+	<h3 class="article-header"><strong>Overview</strong></h3>
+
+	<div class="cd-features">
+		<div class="feature">
+			<h4>Date &amp; Time</h4>
+			<div class="value">Saturday, November 22nd<br></div>
+			<div class="detail">2pm - 5pm local time</div>
+		</div>
+		<div class="feature">
+			<h4>Event Bonus</h4>
+			<div class="value">x3 Catch Stardust</div>
+		</div>
+		<div class="feature">
+			<h4>Exclusive Charged Move</h4>
+			<div class="value">High Horsepower</div>
+			<div class="detail">100 power, 60 energy</div>
+		</div>
+	</div>
+
+	<a name="checklist"></a>
+	<h3 class="article-header"><strong>League &amp; IV Recommendations</strong></h3>
+
+	<?php require_once 'templates/checklist-template.php'; ?>
+
+	<div class="mega-section">
+		<div class="mega-title">
+			<div class="mega-icon"></div>
+			<h4>Mega Evolve one of these Pokemon during Community Day to earn bonus catch candy!</h4>
+		</div>
+		<div class="mega-list">
+			<div class="mega-item" mega="pidgeot">
+				<div class="mega-image"></div>
+				<div class="mega-label">Pidgeot</div>
+			</div>
+			<div class="mega-item" mega="kangaskhan">
+				<div class="mega-image"></div>
+				<div class="mega-label">Kangaskhan</div>
+			</div>
+			<div class="mega-item" mega="lopunny">
+				<div class="mega-image"></div>
+				<div class="mega-label">Lopunny</div>
+			</div>
+		</div>
+	</div>
+
+	<a name="faq"></a>
+	<div class="faq-item">
+		<h3 class="article-header"><strong>How good is Ursaluna with High Horsepower?</strong></h3>
+		<div class="faq-answer">
+			<p><b>Ursaluna has niche play in Master League formats but its underwhelming movepool holds it in back in general PvP.</b> High Horsepower is a high energy Charged Move comparable to Earth Power. However, Ursaluna's best Fast Move is Tackle, which is too slow to be effective with High Horsepower's energy cost. All together, Ursulana has some play in Master League and Master Premier Classic, but pales in comparison to stronger Ground types like Excadrill, Garchomp, Groudon, and Landorus. Ursaluna currently has no play in Great League or Ultra League.</p>
+			<p>If Ursalana obtains the ability to learn Shadow Claw in the future, it would become much more viable.</p>
+		</div>
+	</div>
+
+	<div class="faq-item">
+		<h3 class="article-header"><strong>Can my Shadow Teddiursa learn High Horsepower if I evolve it during Community Day?</strong></h3>
+		<div class="faq-answer">
+			<p><b>Shadow Pokemon must have Frustration removed in order to learn exclusive moves when evolved during Community Day.</b> Even if you unlock a second Charged Move, a Shadow Ursaluna that still has Frustration will not learn High Horsepower when it is evolved.</p>
+			<p>If you have a Shadow Teddiursa with Frustration, do not evolve it during this Community Day and consider waiting to remove Frustration during the <a href="https://pokemongolive.com/en/post/greedy-gluttons/" target="_blank">Team Rocket takeover</a> beginning November 14th. You can then evolve your Shadow Teddiursa later during the December Community Day event to obtain High Horsepower on your Shadow Ursaluna.</p>
+		</div>
+	</div>
+
+	<div class="faq-item">
+		<h3 class="article-header"><strong>I have a Purified Teddiursa. How can I obtain both Return and High Horsepower for Ursaluna?</strong></h3>
+		<div class="faq-answer">
+			<p><b>To obtain both Return and High Horsepower, you must evolve Purified Ursaring into Purified Ursaluna AFTER the Community Day evolution window.</b> If you evolve a Purified Ursaring during the Community Day evolution window, the exclusive move High Horsepower will always overwrite Return (even if you have unlocked a second Charged Move) and you will be unable to restore Return, even via Elite TM.</p>
+			<p>To obtain High Horsepower on a Purified Ursaluna that knows Return, you must unlock a second Charged Move and use an Elite TM.</p>
+		</div>
+	</div>
+
+	<div class="faq-item">
+		<h3 class="article-header"><strong>How do I evolve Ursaring into Ursaluna after Community Day?</strong></h3>
+		<div class="faq-answer">
+			<p><b>After Community Day, Ursaring will only be able to evolve into Ursaluna during a full moon.</b> Check out the Pokemon GO official blog for <a href="https://pokemongolive.com/en/post/community-day-november-2022-teddiursa/" target="_blank">more details</a>.</p>
+		</div>
+	</div>
+
+	<a name="further-resources"></a>
+	<h3 class="article-header"><strong>Additional Resources</strong></h3>
+
+	<div class="flex further-resource-links">
+		<a href="https://www.stadiumgaming.gg/rank-checker" class="gostadium" target="_blank">Stadium Gaming<br>Rank Checker</a>
+		<a href="https://pvpivs.com/" target="_blank">pvpivs.com</a>
+	</div>
+
+	<div class="share-link-container">
+		<p>Share this article:</p>
+		<div class="share-link">
+			<input type="text" value="https://pvpoke.com/articles/community-day/22-11-teddiursa/" readonly>
+			<div class="copy">Copy</div>
+		</div>
+	</div>
+</div>
+
+<!-- Scripts -->
+<script>
+	var articleId = "22-11-teddiursa"; // This is used to fetch JSON data for this page's checklist
+</script>
+
+<script src="<?php echo $WEB_ROOT; ?>js/GameMaster.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/pokemon/Pokemon.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/ModalWindow.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/ArticleChecklist.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/TimelineEvent.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/Battle.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/Battle.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/Main.js?v=<?php echo $SITE_VERSION; ?>"></script>
+
+<?php require_once '../../footer.php'; ?>
