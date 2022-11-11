1182d587c44ca6b2c5847b6a904213294bd6e47e
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-11-11 13:30:52 -0600

    Ursaluna rankings and finalized article content

--- src/articles/community-day/22-11-teddiursa.php
+++ src/articles/community-day/22-11-teddiursa.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$META_TITLE = 'Teddirsa Community Day Guide for PvP';
+$META_TITLE = 'Teddiursa Community Day Guide for PvP';
 
-$META_DESCRIPTION = 'Ursulana will be making its debut in Pokemon GO! How does it stand up to the meta and is this bear a necessity for PvP?';
+$META_DESCRIPTION = 'Ursaluna will be making its debut in Pokemon GO! How does it stand up to the meta and is this bear a necessity for PvP?';
 
 $OG_IMAGE = 'https://pvpoke.com/articles/article-assets/community-day/22-11-teddiursa/og.jpg';
 
@@ -12,7 +12,7 @@ require_once '../../header.php';
 ?>
 
 <div class="section article white litwick">
-	<h1>Ursaluna Community Day Guide for PvP</h1>
+	<h1>Teddiursa Day Guide for PvP</h1>
 	<div class="date">Last updated November 11, 2022</div>
 	<img src="<?php echo $WEB_ROOT;?>articles/article-assets/community-day/22-11-teddiursa/banner.jpg" />
 	<p>Teddiursa is moonlighting as a featured Pokemon this Community Day, alongside the debut of Ursaring's new evolution, Ursaluna. This Ground/Normal type can reach a high CP for Master League, but does it have the moves to pull it off? We'll cover Ursaluna's PvP value below as well as tips for evolving. You can find more info on Teddiursa Community Day at the <a href="https://pokemongolive.com/en/post/community-day-november-2022-teddiursa/" target="_blank">Pokemon GO blog</a>. Below are the leagues and IV spreads to look out for, and answers to frequently asked questions.</p>
@@ -23,7 +23,7 @@ require_once '../../header.php';
 	<div class="cd-features">
 		<div class="feature">
 			<h4>Date &amp; Time</h4>
-			<div class="value">Saturday, November 22nd<br></div>
+			<div class="value">Saturday, November 12th<br></div>
 			<div class="detail">2pm - 5pm local time</div>
 		</div>
 		<div class="feature">
@@ -67,24 +67,25 @@ require_once '../../header.php';
 	<div class="faq-item">
 		<h3 class="article-header"><strong>How good is Ursaluna with High Horsepower?</strong></h3>
 		<div class="faq-answer">
-			<p><b>Ursaluna has niche play in Master League formats but its underwhelming movepool holds it in back in general PvP.</b> High Horsepower is a high energy Charged Move comparable to Earth Power. However, Ursaluna's best Fast Move is Tackle, which is too slow to be effective with High Horsepower's energy cost. All together, Ursulana has some play in Master League and Master Premier Classic, but pales in comparison to stronger Ground types like Excadrill, Garchomp, Groudon, and Landorus. Ursaluna currently has no play in Great League or Ultra League.</p>
-			<p>If Ursalana obtains the ability to learn Shadow Claw in the future, it would become much more viable.</p>
+			<p><b>Ursaluna has decent play in Master League formats as a non-Legendary Ground option.</b> High Horsepower is a high energy Charged Move comparable to Earth Power, and both Thunder Punch and Fire Punch provide cheap coverage options. Ursaluna's best Fast Move is Tackle, which may generate energy too slowly to be truly effective. All together, Ursaluna is viable in Master League and Master Premier Classic, but falls short of stronger Ground options like Excadrill, Garchomp, Groudon, and Landorus.</p>
+			<p>Ursaluna currently doesn't have the stats or moves to be viable in Great League or Ultra League.</p>
 		</div>
 	</div>
 
 	<div class="faq-item">
 		<h3 class="article-header"><strong>Can my Shadow Teddiursa learn High Horsepower if I evolve it during Community Day?</strong></h3>
 		<div class="faq-answer">
-			<p><b>Shadow Pokemon must have Frustration removed in order to learn exclusive moves when evolved during Community Day.</b> Even if you unlock a second Charged Move, a Shadow Ursaluna that still has Frustration will not learn High Horsepower when it is evolved.</p>
+			<p><b>Shadow Pokemon must have Frustration removed in order to learn exclusive moves during Community Day.</b> Even if you unlock a second Charged Move, a Shadow Ursaluna that still has Frustration will not learn High Horsepower when it is evolved.</p>
 			<p>If you have a Shadow Teddiursa with Frustration, do not evolve it during this Community Day and consider waiting to remove Frustration during the <a href="https://pokemongolive.com/en/post/greedy-gluttons/" target="_blank">Team Rocket takeover</a> beginning November 14th. You can then evolve your Shadow Teddiursa later during the December Community Day event to obtain High Horsepower on your Shadow Ursaluna.</p>
 		</div>
 	</div>
 
 	<div class="faq-item">
-		<h3 class="article-header"><strong>I have a Purified Teddiursa. How can I obtain both Return and High Horsepower for Ursaluna?</strong></h3>
+		<h3 class="article-header"><strong>I have a Purified Teddiursa. Is Return any good and how can I obtain both Return and High Horsepower for Ursaluna?</strong></h3>
 		<div class="faq-answer">
-			<p><b>To obtain both Return and High Horsepower, you must evolve Purified Ursaring into Purified Ursaluna AFTER the Community Day evolution window.</b> If you evolve a Purified Ursaring during the Community Day evolution window, the exclusive move High Horsepower will always overwrite Return (even if you have unlocked a second Charged Move) and you will be unable to restore Return, even via Elite TM.</p>
-			<p>To obtain High Horsepower on a Purified Ursaluna that knows Return, you must unlock a second Charged Move and use an Elite TM.</p>
+			<p>Return has some play for Ursaluna in Master League, hitting a variety of Pokemon for strong neutral damage. As a trade off, Return makes Ursaluna considerably slower and reduces its effectiveness while shields are in play.</p>
+			<p><b>To keep Return on a Purified Ursaring, you must evolve it into Purified Ursaluna AFTER the Community Day evolution window (10pm local time).</b> If you evolve a Purified Ursaring during the Community Day evolution window, the exclusive move High Horsepower will always overwrite Return, even if you have unlocked a second Charged Move. If this happens, you will be unable to restore Return, even via Elite TM.</p>
+			<p>To obtain High Horsepower on a Purified Ursaluna that knows Return, you must unlock a second Charged Move and use an Elite TM. However, if Return becomes a preferred move on Ursaluna, it's unlikely that High Horsepower will be the move of choice alongside Return since both are high energy moves.</p>
 		</div>
 	</div>
 
--- src/index.php
+++ src/index.php
@@ -66,17 +66,18 @@
 
 	<div class="article-item flex">
 		<div class="col-3">
-			<a href="<?php echo $WEB_ROOT; ?>articles/community-day/22-10-litwick/">
-				<img src="<?php echo $WEB_ROOT; ?>articles/article-assets/community-day/22-10-litwick/thumb.jpg">
+			<a href="<?php echo $WEB_ROOT; ?>articles/community-day/22-11-teddiursa/">
+				<img src="<?php echo $WEB_ROOT; ?>articles/article-assets/community-day/22-11-teddiursa/thumb.jpg">
 			</a>
 		</div>
 		<div class="col-9">
-			<h4><a href="<?php echo $WEB_ROOT; ?>articles/community-day/22-10-litwick/">Litwick Community Day Guide for PvP</a></h4>
-			<div class="date">October 12, 2022</div>
-			<p>Will Litwick be able to haunt its way into PvP this Community Day? Let's shed some light on how Chandelure performs in PvP!</p>
+			<h4><a href="<?php echo $WEB_ROOT; ?>articles/community-day/22-11-teddiursa/">Teddiursa Community Day Guide for PvP</a></h4>
+			<div class="date">November 11, 2022</div>
+			<p>Ursulana will be making its debut in Pokemon GO! How does it stand up to the meta and is this bear a necessity for PvP?</p>
 			<div class="tags"><a href="<?php echo $WEB_ROOT; ?>articles?tag=Community Day"># Community Day</a><a href="<?php echo $WEB_ROOT; ?>articles?tag=Infographic"># Infographic</a></div>
 		</div>
 	</div>
+
 </div>
 
 <?php
