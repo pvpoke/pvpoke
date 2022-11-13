e9dde8127d4f0c7ea3fdb98a53ab17b034c2d1f8
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-11-13 09:11:45 -0600

    Speculative Shadow article

---
+++ src/articles/infographics/22-11-shadow-predictions.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$META_TITLE = 'PvPoke\'s Shadow Crystal Ball';
+
+$META_DESCRIPTION = 'Which Shadow Pokemon could become more relevant for PvP in a future move update or Community Day? Check out this speculative list if you have TM\'s to spare!';
+
+require_once '../../header.php';
+
+?>
+
+<div class="section article white">
+	<h1>PvPoke's Shadow Crystal Ball</h1>
+	<div class="date">Last updated November 13, 2022</div>
+	<a href="<?php echo $WEB_ROOT;?>articles/article-assets/infographics/22-11-shadow-predictions/Shadow-Crystal-Ball.jpg" target="_blank">
+		<img style="margin-bottom:15px" src="<?php echo $WEB_ROOT;?>articles/article-assets/infographics/22-11-shadow-predictions/Shadow-Crystal-Ball.jpg" />
+	</a>
+
+</div>
+
+  <div class="share-link-container">
+		<p>Share this article:</p>
+		<div class="share-link">
+			<input type="text" value="https://pvpoke.com/articles/infographics/22-11-shadow-predictions/" readonly>
+			<div class="copy">Copy</div>
+		</div>
+	</div>
+<?php require_once '../../footer.php'; ?>
