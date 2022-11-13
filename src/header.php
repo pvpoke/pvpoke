f6299d62615470dfe4943c3b06856653b4f738aa
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-11-13 13:51:24 -0600

    Added new Shadows, updated rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.14.9';
+$SITE_VERSION = '1.29.14.10';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
