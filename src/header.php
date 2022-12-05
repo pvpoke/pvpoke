a3cfc58639026c9718df2441b767b9724ca3d69b
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-12-05 10:32:40 -0600

    Added Keldeo and Crabominable, updated Ultra Premier Classic

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.16.4';
+$SITE_VERSION = '1.29.17';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
