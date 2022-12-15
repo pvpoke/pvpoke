c401be6ab4da1da8581c61ec4af5870dabf68c14
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2022-12-15 17:40:39 -0600

    Added Vivillion to rankings, Glaciate stats

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.20.1';
+$SITE_VERSION = '1.29.20.2';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
