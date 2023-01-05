6e3553f893c40dc8502fb4d89036fc1eb115226d
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-01-05 10:19:30 -0600

    Added Mega Salamence, Fusion Bolt to Zekrom

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.21.5';
+$SITE_VERSION = '1.29.21.6';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
