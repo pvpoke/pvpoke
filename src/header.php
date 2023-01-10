92f244ea3bd5da231c7f4eea8f076c7b84bedb7f
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-01-10 08:05:08 -0600

    Added Mega Salamence to Factions Master League

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.21.7';
+$SITE_VERSION = '1.29.21.8';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
