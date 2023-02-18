12251f21458cde33bf87d859b1361ce463475b06
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-02-18 15:01:29 -0600

    Updated Master League training analysis

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.25';
+$SITE_VERSION = '1.29.25.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
