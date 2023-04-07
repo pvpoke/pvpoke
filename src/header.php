49ea64278bc97c3df9dfcc409fb7ba1c23a52c10
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-04-07 09:39:37 -0500

    Added Regieleki

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.29.27.2';
+$SITE_VERSION = '1.29.27.4';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
