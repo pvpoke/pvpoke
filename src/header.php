4ae18cf235ea0a5225472a3a3e8054bf4d0d2408
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-07-28 16:47:22 -0500

    Water Shuriken stats

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.6.8';
+$SITE_VERSION = '1.30.6.10';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
