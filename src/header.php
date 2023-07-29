867bc6395c8f0284254f0f4d017a847ba979657f
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-07-29 12:37:12 -0500

    Updated rankings for Poliwrath and Politoed CD

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.6.10';
+$SITE_VERSION = '1.30.6.12';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
