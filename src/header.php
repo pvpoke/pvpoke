86e22f36cbe24b08f94ebeedb0b371f0c027689b
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-08-18 09:12:06 -0500

    Updated Haxorus moveset for Fantasy Cup

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.6.20';
+$SITE_VERSION = '1.30.6.22';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
