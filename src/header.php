c61c69d9005ac848a0e268ef01464e2ddc4cd68d
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-09-01 17:24:47 -0500

    Updated Gymbreakers and Battle Frontier rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.9';
+$SITE_VERSION = '1.30.10.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
