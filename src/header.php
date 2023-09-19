415041528a8022a60f0273acba9bcfd147d79d2c
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-09-19 08:40:17 -0500

    Added Volt Switch to Charjabug and Vikavolt

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.13.8';
+$SITE_VERSION = '1.30.13.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
