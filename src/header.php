da32198e7cb02568f1e8c717f76cb90fcb1ecf61
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-09-20 13:31:38 -0500

    Added Play Pokemon meta group

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.13.10';
+$SITE_VERSION = '1.30.13.11';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
