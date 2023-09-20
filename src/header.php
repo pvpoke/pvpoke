9565b4ba82ae7beed3863cf2261330ddb68ce3ca
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-09-20 12:52:27 -0500

    Updated ML training analysis, Rhyperior moveset in ML

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.13.9';
+$SITE_VERSION = '1.30.13.10';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
