de893f9338e99a9b1a4c37a1e4a511fc39e7db39
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-09-22 17:29:16 -0500

    Added Deoxys to Gymbreakers Invitational

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.13.14';
+$SITE_VERSION = '1.30.13.15';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
