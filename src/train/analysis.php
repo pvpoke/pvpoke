27d1b6b47027b6f238afac535d9f34ae7712df18
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-10-02 19:52:52 -0500

    Updated UL training teams and analysis, added Sunshine training

--- src/train/analysis.php
+++ src/train/analysis.php
@@ -93,7 +93,6 @@ require_once '../header.php';
 		<option value="1500" cup="all" level-cap="50">Great League</option>
 		<option value="2500" cup="all" level-cap="50">Ultra League</option>
 		<option value="10000" cup="all" level-cap="50">Master League</option>
-		<option value="1500" cup="psychic" level-cap="50">Psychic Cup Remix</option>
 	</select>
 
 	<div class="date-updated">Last updated</div>
