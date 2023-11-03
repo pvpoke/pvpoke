6ddc583eabb42c918f993e8191dcceae9f3da500
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-11-03 17:50:32 -0500

    Updated Great League top performers

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.16';
+$SITE_VERSION = '1.30.16.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
