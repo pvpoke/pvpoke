457f0657bcd4569fd0cba9d313e190c3fa89a519
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-11-09 18:10:58 -0600

    Added Mega Garchomp to base gamemaster

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.30.16.4';
+$SITE_VERSION = '1.30.16.5';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
