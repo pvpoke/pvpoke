518d753ed9b57012fd06d67a9420481d270b2f0e
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2023-11-11 19:57:56 -0600

    Fixed RSS Feed editor

--- src/rss/feedEditor.php
+++ src/rss/feedEditor.php
@@ -39,55 +39,6 @@ require_once '../header.php';
 <script src="<?php echo $WEB_ROOT; ?>js/GameMaster.js?v=<?php echo $SITE_VERSION; ?>"></script>
 <script src="<?php echo $WEB_ROOT; ?>js/pokemon/Pokemon.js?v=<?php echo $SITE_VERSION; ?>"></script>
 <script src="<?php echo $WEB_ROOT; ?>js/interface/RSSReader.js?v=<?php echo $SITE_VERSION; ?>"></script>
-
-<script>
-
-$(function() {
-
-	var xml;
-
-	function getXMLData(data){
-		xml = data;
-		updateXMLDisplay();
-	}
-
-	var rss = RSS.getInstance(getXMLData)
-
-
-	$("#add-post").click(function(e){
-
-		var pubDate = new Date().toUTCString();
-
-		$(xml).find("channel").append("<item><title>"+$("#post-title").val()+"</title><description>"+$("#post-content").val()+"</description><link>"+$("#post-link").val()+"</link><pubDate>"+pubDate+"</pubDate></item>");
-
-		updateXMLDisplay();
-	});
-
-	function updateXMLDisplay(){
-		$("textarea.export").val((new XMLSerializer()).serializeToString(xml));
-
-		var items = rss.feedToObjects(xml);
-
-		$(".feed").html("");
-
-		for(var i = 0; i < items.length; i++){
-			var $html = rss.generateItemHTML(items[i]);
-			$(".feed").append($html);
-		}
-	}
-
-	$(".export-xml").click(function(e){
-		e.preventDefault();
-
-		var el = $(e.target).prev()[0];
-		el.focus();
-		el.setSelectionRange(0, el.value.length);
-		document.execCommand("copy");
-	});
-
-});
-
-
-</script>
+<script src="<?php echo $WEB_ROOT; ?>js/devtools/RSSFeedInterface.js?v=<?php echo $SITE_VERSION; ?>"></script>
 
 <?php require_once '../footer.php'; ?>
