855e12b252004737c72a7a82c24bf44c78694fb6
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-01-18 17:35:14 -0600

    Added Annihilape to Play Pokemon meta

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.5.2';
+$SITE_VERSION = '1.31.5.4';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
