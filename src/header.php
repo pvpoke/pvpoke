2a0f106d0fa9e3ab8b8e35a9276cf80984b7dfec
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-01-30 04:14:18 -0600

    Fixed Hisuian Decidueye stats

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.5.12';
+$SITE_VERSION = '1.31.5.13';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
