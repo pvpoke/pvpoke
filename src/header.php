a93e86ee8fbcfcf82199155d73e6ddf763e6a5aa
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-02-07 10:39:44 -0600

    Added Hisuian Decidueye

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.5.16';
+$SITE_VERSION = '1.31.5.17';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
