dfd28cabe28a88dee007321dbb5f27652cd72b7f
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-02-12 17:44:15 -0600

    Fixed Enamorus Therian stats, added speculative Springtide Storm

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.5.20';
+$SITE_VERSION = '1.31.5.21';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
