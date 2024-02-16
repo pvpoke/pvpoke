e75593c843489d900711a6dfd38f680c60cfde3b
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-02-16 17:07:10 -0600

    Updated Little Catch Cup rules, rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.5.22';
+$SITE_VERSION = '1.31.5.24';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
