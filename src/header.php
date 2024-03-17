6b4f85f26e68617855722583eada29b9fe285252
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-03-17 17:50:44 -0500

    Ultra Premier rankings and meta group

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.7.8';
+$SITE_VERSION = '1.31.7.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
