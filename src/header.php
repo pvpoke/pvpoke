e80375a8654d9bbebf1f9660d1d5989deb60bad1
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-03-20 08:18:50 -0500

    Tapu Lele Nature's Madness, new Shadows

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.7.9';
+$SITE_VERSION = '1.31.7.10';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
