6904c5cc935c2191a50a0ec681c3c9c61419f266
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-04-10 12:39:00 -0500

    Updated Swampert and Clodsire movesets, update notes

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.7.18';
+$SITE_VERSION = '1.31.7.19';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
