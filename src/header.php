51a65e220695c96efba3b51f33c408164f3f77d8
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-04-27 06:50:07 -0500

    Devon Genetic Cup rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.8';
+$SITE_VERSION = '1.31.8.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
