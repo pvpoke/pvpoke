df14dab94f3b86c2624965785feb9762c35be524
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-05-20 15:27:26 -0500

    Catch Cup rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.8.6';
+$SITE_VERSION = '1.31.8.7';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
