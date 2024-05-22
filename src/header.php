3648abe3c62a42fd87f5203cd92b417379c96b88
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-05-22 10:08:38 -0500

    Added Naganadel, Blacephalon, and Stakataka

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.8.7';
+$SITE_VERSION = '1.31.8.8';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
