ac3598918a306fee6b48884a87f61b5590f1a60e
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-05-31 12:37:07 -0500

    Added Ultra Premier to master

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.9.1';
+$SITE_VERSION = '1.31.9.2';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
