8cca3bd7bcd221123559c729171069ea02cf2991
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-06-24 14:04:35 -0500

    Fixed Genetic Remix, updated Great League top performers

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.10.6';
+$SITE_VERSION = '1.31.10.7';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
