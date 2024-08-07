c1051c2aa8c442a621b0d3017cb104f40704bdf7
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-08-07 09:33:36 -0500

    New Shadow rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.31.11.8';
+$SITE_VERSION = '1.31.11.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
