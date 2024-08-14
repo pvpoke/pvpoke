1af38fe604910be52a0c474f21c39b449d7daffd
Michał Wadas <michalwadas@gmail.com>
2024-08-15 01:13:02 +0200

    refactor: replace TimelineAction and TimelineEvent with classes typed in JsDoc

--- src/js/battle/TimelineEvent.js
+++ src/js/battle/TimelineEvent.js
@@ -1,13 +1,57 @@
-// JavaScript Document
+/**
+ * @typedef {
+ *      'switchAvailable' | 'faint' | 'tap interaction' |
+ *      'tap interaction wait' | `tap{string}` | 'shield'
+ *      } TimelineEventType
+ */
 
-function TimelineEvent(type, name, actor, time, turn, values){
+/**
+ * An event that occurs in the timeline
+ */
 
-	values = typeof values !== 'undefined' ? values : [0];
+class TimelineEvent {
+	/**
+	 * @type {TimelineEventType}
+	 */
+	type;
+	/**
+	 * @type {string}
+	 */
+	name;
 
-	this.type = type;
-	this.name = name;
-	this.actor = actor;
-	this.time = time;
-	this.turn = turn;
-	this.values = values; // 0 - damage, 1 - energy
+	actor;
+
+	/**
+	 * @type {number}
+	 */
+	time;
+
+	/**
+	 * @type {number}
+	 */
+	turn;
+
+	/**
+	 * @type {Array<number>}
+	 */
+	values;
+
+	/**
+	 *
+	 * @param {TimelineEventType} type
+	 * @param {string} name
+	 * @param actor
+	 * @param {number} time
+	 * @param {number} turn
+	 * @param {Array<number>} [values]
+	 */
+	constructor(type, name, actor, time, turn, values) {
+		values = typeof values !== "undefined" ? values : [0];
+		this.type = type;
+		this.name = name;
+		this.actor = actor;
+		this.time = time;
+		this.turn = turn;
+		this.values = values; // 0 - damage, 1 - energy
+	}
 }
