ebfd5c54321fa742df95c3b15c25ab204b6ab71e
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-08-16 11:31:23 -0500

    Great League starter pack

---
+++ src/articles/infographics/24-08-shared-skies-great-league-starter-pack.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$META_TITLE = 'Great League Starter Pack for Season of Shared Skies';
+
+$META_DESCRIPTION = 'Check out the premiere and budget picks to get started in Great League and GO Battle League for Season of Shared Skies!';
+
+$OG_IMAGE = 'https://pvpoke.com/articles/article-assets/infographics/24-08-shared-skies-great-league-starter-pack/og.jpg';
+
+require_once '../../header.php';
+
+?>
+
+<div class="section article white">
+	<h1>Great League Starter Pack for Season of Adventures Abound</h1>
+	<div class="date">Last updated August 16, 2024</div>
+	<p>Two of these teams come from videos by <a href="https://www.youtube.com/watch?v=CrN-KXYwuw4" target="_blank">ItsAXN</a> and <a href="https://www.youtube.com/watch?v=V0TEnux3X4c" target="_blank">PurpleKyogre</a>. Check them out!</p>
+	<a href="<?php echo $WEB_ROOT;?>articles/article-assets/infographics/24-08-shared-skies-great-league-starter-pack/great-league-starter-pack-shared-skies.jpg" target="_blank">
+		<img src="<?php echo $WEB_ROOT;?>articles/article-assets/infographics/24-08-shared-skies-great-league-starter-pack/great-league-starter-pack-shared-skies.jpg" />
+	</a>
+
+</div>
+
+  <div class="share-link-container">
+		<p>Share this article:</p>
+		<div class="share-link">
+			<input type="text" value="https://pvpoke.com/articles/infographics/24-08-shared-skies-great-league-starter-pack/" readonly>
+			<div class="copy">Copy</div>
+		</div>
+	</div>
+<?php require_once '../../footer.php'; ?>
