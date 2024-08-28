6d524d6e57cf1b40a4198d36ac9bd23b494d5c54
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-08-27 19:47:42 -0500

    Updated Rankings for Gen 8, added back Master Premier listing

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.32.0.3';
+$SITE_VERSION = '1.32.0.4';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
