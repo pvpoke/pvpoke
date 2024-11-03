1f30febf1ad61169d7d18798fe9acc986d755250
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-11-03 12:45:40 -0600

    Initial Mankey Community Day article content

---
+++ src/articles/community-day/24-11-mankey.php
@@ -0,0 +1,127 @@
+<?php
+
+
+$META_TITLE = 'Mankey Community Day Guide for PvP';
+
+$META_DESCRIPTION = 'Mankey Community Day is a massive event for PvP! Rage Fist is a big upgrade for both Annihilape and Primeape. Here\'s all you need to know and what to look for this weekend!';
+
+$OG_IMAGE = 'https://pvpoke.com/articles/article-assets/community-day/24-11-mankey/og.jpg';
+
+require_once '../../header.php';
+
+?>
+
+<div class="section article white poliwag">
+	<h1>Mankey Community Day Guide for PvP</h1>
+	<div class="date">Last updated November 6th, 2024</div>
+	<img src="<?php echo $WEB_ROOT;?>articles/article-assets/community-day/24-11-poliwag/banner.jpg" />
+	<p>The Poliwag family is getting its time to shine! Poliwrath will learn Counter and Politoed will learn Ice Beam this upcoming Community Day. With multiple evolutions, leagues, and even Shadows, there will be a lot to chase. How will each Pokemon improve and what should you look out for? Let's dive into the details below! You can find more info on Mankey Community Day at the <a href="https://pokemongolive.com/post/communityday-november-2024-mankey?hl=en" target="_blank">Pokemon GO blog</a>. </p>
+
+	<a name="overview"></a>
+	<h3 class="article-header"><strong>Overview</strong></h3>
+
+	<div class="cd-features">
+		<div class="feature">
+			<h4>Date &amp; Time</h4>
+			<div class="value">Sunday, November 10th<br></div>
+			<div class="detail">2pm - 5pm local time</div>
+		</div>
+		<div class="feature">
+			<h4>Event Bonus</h4>
+			<div class="value">3x Catch XP</div>
+		</div>
+		<div class="feature">
+			<h4>Exclusive Charged Move</h4>
+			<div class="value">Rage Fist</div>
+			<div class="detail">50 power, increases Attack by one stage<br>Learned by both Annihilape and Primeape</div>
+		</div>
+	</div>
+
+	<a name="checklist"></a>
+	<h3 class="article-header"><strong>League &amp; IV Recommendations</strong></h3>
+
+	<?php require_once 'templates/checklist-template.php'; ?>
+
+	<div class="mega-section">
+		<div class="mega-title">
+			<div class="mega-icon"></div>
+			<h4>Mega Evolve one of these Pokemon during Community Day to earn bonus catch candy!</h4>
+		</div>
+		<div class="mega-list">
+			<div class="mega-item" mega="medicham">
+				<div class="mega-image"></div>
+				<div class="mega-label">Medicham</div>
+			</div>
+			<div class="mega-item" mega="lopunny">
+				<div class="mega-image"></div>
+				<div class="mega-label">Lopunny</div>
+			</div>
+			<div class="mega-item" mega="lucario">
+				<div class="mega-image"></div>
+				<div class="mega-label">Lucario</div>
+			</div>
+		</div>
+	</div>
+
+	<a name="faq"></a>
+	<div class="faq-item">
+		<h3 class="article-header"><strong>How good is Annihilape with Rage Fist?</strong></h3>
+		<div class="faq-answer">
+			<p><b>Annihilape should return to the meta with Rage Fist and could be top meta again after future Fast Move updates.</b> Annihilape immediately became a top meta Pokemon after its debut, but took a hard hit from the Counter nerf this season. With Rage Fist, Annihilape should see play again in all three leagues, although it probably won't return to its previous apex without further move updates. Rage Fist Annihilape is coming at a time when the Fighting-type field is notably thin, so its resurgence could help fill a gap that's been open since the latest meta shift.</p>
+			<p>Annihilape previously ran Night Slash and Shadow Ball as its preferred Charged Attacks. Since Rage Fist is a Ghost-type attack with a guaranteed Attack boost, it may discard both of these moves for the STAB combo of Rage Fist + Close Combat. Ice Punch still exists as a coverage move depending on the meta, but it's a less likely option.</p>
+		</div>
+	</div>
+
+	<div class="faq-item">
+		<h3 class="article-header"><strong>How good is Primeape with Rage Fist? Is it better or worse than Annihilape?</strong></h3>
+		<div class="faq-answer">
+			<p><b>Rage Fist Primeape looks more dangerous than Annihilape on paper and could be the top Fighting type in the short term, but has fewer prospects long term.</b> With its legacy attack Karate Chop, Primeape significantly outspeed Annihilape. Although it lacks STAB, it can still stack Rage Fist buffs to lethal effect. That said, pure Fighting types like Annihilape rarely find a foothold in the open metagame. Even its closest current comparison, Machamp, doesn't see notable play amid a dearth of viable Fighting types. Ultimately, Primeape could be better than Annihilape to start, but Annihilape's stats and dual typing give it more potential for the future.</p>
+			<p>Like Annihilape, Primeape has a wide movepool and is most likely to settle on Rage Fist + Close Combat as its preferred moveset. Karate Chop has less Fast Attack pressure than Counter and Primeape's Rage Fist will lack STAB, so it may be more dependent on Close Combat than Annihilape to secure its matchups.</p>
+		</div>
+	</div>
+
+	<div class="faq-item">
+		<h3 class="article-header"><strong>My Shadow Mankey still has Frustration. Can it learn a Community Day move if I evolve it during the event?</strong></h3>
+		<div class="faq-answer">
+			<p><b>No, Shadow Pokemon with Frustration cannot learn an exclusive Charged Move when it evolves, </b> even if it has a 2nd Charged Move unlocked. You must have removed Frustration during a previous Team Rocket event.</p>
+		</div>
+	</div>
+
+	<div class="faq-item">
+		<h3 class="article-header"><strong>What will happen if I evolve a Purified Mankey during Community Day? Is there a way to obtain both Rage Fist and Return?</strong></h3>
+		<div class="faq-answer">
+			<p><b>Exclusive Charged Moves will replace Return if you evolve a Purified Pokemon during its Community Day event.</b> To obtain both Rage Fist and Return, you must evolve your Pokemon after the event's evolution window (10pm local time), unlock a 2nd Charged Move, and use an Elite TM to obtain Rage Fist.</p>
+		</div>
+	</div>
+
+	<a name="further-resources"></a>
+	<h3 class="article-header"><strong>Additional Resources</strong></h3>
+
+	<div class="flex further-resource-links">
+		<a href="https://pvpivs.com/" target="_blank">pvpivs.com</a>
+	</div>
+
+	<div class="share-link-container">
+		<p>Share this article:</p>
+		<div class="share-link">
+			<input type="text" value="https://pvpoke.com/articles/community-day/23-11-mankey/" readonly>
+			<div class="copy">Copy</div>
+		</div>
+	</div>
+</div>
+
+<!-- Scripts -->
+<script>
+	var articleId = "24-11-mankey"; // This is used to fetch JSON data for this page's checklist
+</script>
+
+<script src="<?php echo $WEB_ROOT; ?>js/GameMaster.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/pokemon/Pokemon.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/ModalWindow.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/ArticleChecklist.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/TimelineEvent.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/Battle.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/battle/Battle.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/Main.js?v=<?php echo $SITE_VERSION; ?>"></script>
+
+<?php require_once '../../footer.php'; ?>
