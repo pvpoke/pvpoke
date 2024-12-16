dccd3a59444647d841f1d25098a62370dae75fe0
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2024-12-15 21:23:04 -0600

    Fixed Pillar Cup Pokemon, Necrozma fix
    Removed Moongeist Beam and Sunsteel Strike from Elite TM pool.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.32.6.4';
+$SITE_VERSION = '1.32.6.5';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
