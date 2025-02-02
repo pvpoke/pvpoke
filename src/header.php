8976d942d1e17f5e87c30c75dcf0ab3160ba8b69
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-02-02 12:24:35 -0600

    Removed Gligar and Galarian Stunfisk from Little Jungle Cup, Love Cup rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.32.6.21';
+$SITE_VERSION = '1.32.6.22';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
