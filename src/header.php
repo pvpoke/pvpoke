9d1bda973b533b4ce19473913fd8175a8f6e54be
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-02-19 10:20:33 -0600

    Black and White Kyurem rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.32.6.28';
+$SITE_VERSION = '1.32.6.29';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
