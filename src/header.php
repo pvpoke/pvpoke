f50e6e23041020b95924f7a3a4e3a571898ff105
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-02-21 09:42:23 -0600

    Updated Zapdos moveset

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.32.6.30';
+$SITE_VERSION = '1.32.6.31';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
