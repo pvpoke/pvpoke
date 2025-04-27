5d690c3d7c26b21b70dbc30993e33e35a94220a5
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-04-27 16:59:05 -0500

    Top performers updates, added Avalanche to Vanilluxe

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.32.9.26';
+$SITE_VERSION = '1.32.9.27';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
