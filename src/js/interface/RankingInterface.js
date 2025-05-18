ccdaca4621053e35c044b399e1aa690db50641ee
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-05-17 22:10:39 -0500

    Styling tweaks

--- src/js/interface/RankingInterface.js
+++ src/js/interface/RankingInterface.js
@@ -359,7 +359,7 @@ var InterfaceMaster = (function () {
 
 					var typeStr = pokemon.types[i].charAt(0).toUpperCase() + pokemon.types[i].slice(1);
 					if(pokemon.types[i] != "none"){
-						$el.find(".type-container").append("<div class=\"type-info "+pokemon.types[i]+"\">"+typeStr+"</div>");
+						$el.find(".type-container").append("<div class=\"type-info\">"+typeStr+"</div>");
 					}
 				}
 
