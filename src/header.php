22abb8c7b0d751cf61652718068f6aa69484464c
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-06-27 18:01:57 -0500

    Hisui Cup rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.33.2.7';
+$SITE_VERSION = '1.33.2.8';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
