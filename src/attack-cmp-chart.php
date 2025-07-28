b7c1788acc9641ff20d3ad93dd3f76b359f5e14d
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-07-28 16:12:34 -0500

    Initial commit
    Initial interface, functionality, and styling.

---
+++ src/attack-cmp-chart.php
@@ -0,0 +1,123 @@
+<?php
+
+$cp = '1500';
+$cup = 'all';
+
+if(isset($_GET['cp'])){
+	$cp = htmlspecialchars($_GET['cp']);
+}
+
+if(isset($_GET['cup'])){
+	$cup = htmlspecialchars($_GET['cup']);
+}
+
+$CANONICAL = '/attack-cmp-chart/' . $cup . '/' . $cp;
+
+$league = 'Great League';
+
+switch($cp){
+	case "500":
+		$league = 'Little Cup';
+		break;
+
+	case "1500":
+		$league = 'Great League';
+		break;
+
+	case "2500":
+		$league = 'Ultra League';
+		break;
+
+	case "10000":
+		$league = 'Master League';
+		break;
+}
+
+$META_TITLE = 'Attack Stat (CMP) Chart | ' . $league;
+
+$META_DESCRIPTION = 'See the Attack stat ranges of Pokemon in ' . $league . ' and which Pokemon win Charged Move Priority (CMP). The Pokemon with the higher Attack stat goes first.';
+
+require_once 'header.php';
+
+?>
+
+<h1>Attack Stat (CMP) Chart</h1>
+<div class="section white">
+
+	<?php require_once 'modules/formatselect.php'; ?>
+
+	<p>When two Charged Moves occur on the same turn, the Pokemon with the highest Attack stat goes first. This is usually referred to as Charged Move Priority (CMP) or Charged Attack Priority (CAP). The chart below shows the minimum and maximum Attack stat values for each Pokemon. Click or hover to see which Pokemon fall within the same Attack range.</p>
+
+	<div class="poke-search-container">
+		<input class="poke-search" target="teams" type="text" placeholder="Search Pokemon" />
+		<a href="#" class="search-info">i</a>
+	</div>
+
+	<div class="table-container cmp-chart-container">
+		<table class="train-table cmp-chart" cellspacing="0">
+			<thead>
+				<tr>
+					<th class="poke-name"><a href="#" data="pokemon">Pokemon</a></th>
+					<th><a href="#" class="selected" data="attack">Attack Stat Range</a></th>
+				</tr>
+				<!--Row html to clone-->
+				<tr class="hide">
+					<td class="poke-name"><div class="name"></div></td>
+					<td class="attack-range">
+						<div class="cmp-item">
+							<div class="min"></div>
+							<div class="max"></div>
+						</div>
+					</td>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="share-link-container">
+		<p>Share this chart:</p>
+		<div class="share-link">
+			<input type="text" value="" readonly>
+			<div class="copy">Copy</div>
+		</div>
+	</div>
+</div>
+
+<?php require 'modules/ads/body-728.php'; ?>
+
+<div class="section white custom-rankings-meta-group">
+	<h3>Pokemon List</h3>
+	<p>Customize the list below to add or remove Pokemon from the chart. Note that links shared from this page only show the default list.</p>
+	<?php require 'modules/pokemultiselect.php'; ?>
+</div>
+
+<div class="section about white">
+	<a class="toggle" href="#">About Charged Move Priority <span class="arrow-down">&#9660;</span><span class="arrow-up">&#9650;</span></a>
+	<div class="toggle-content">
+		<p>This page offers another way to analyze the PvP meta to find Pokemon and teams that may work for you. Knowing what the data represents will help you make the most of it.</p>
+		<h2>Where does this data come from?</h2>
+		<p>Data is sampled from <a href="<?php echo $WEB_ROOT; ?>train/">Training Battles</a> people play against the site's bot. The sampled data is typically over a 7-day period. It includes Pokemon and team data as played by both players and the bot. While it doesn't directly represent battles from the game, it is a close approximation.</p>
+		<p>The data is sampled with a threshold of around 150 minimum games for Pokemon, and 20 games minimum for teams. Pokemon and teams near these thresholds are marked orange to highlight their small sample sizes. Pokemon or teams with small sample sizes are more prone to being outliers (whether overperforming or underperforming). Consider these data points with some healthy skepticism.</p>
+		<h3>How does this page differ from the regular rankings?</h3>
+		<p>The regular rankings are generated using 1 vs 1 simulations between eligible Pokemon. The results are empirical and repeatable, but don't take into account team composition or full dynamic play like switching or failed/successful baits. It can provide immediate results for new Pokemon or move updates.</p>
+		<p>The Training Analysis data is recorded from fully played games from the Train feature. Team composition, player decisions, and dynamic play are all taken into account. It is able to provide a fuller picture than the simulated rankings. However, the data is not empirical. Performance and usage numbers are subject to the players who use the Train feature, how frequently they use particular Pokemon, and their performance with those Pokemon. Training Analysis also cannot provide immediate data for new Pokemon or move updates; a sufficient volume of battles must be recorded first.</p>
+	</div>
+</div>
+
+<?php require_once 'modules/search-string-help.php'; ?>
+
+<?php require_once 'modules/scripts/battle-scripts.php'; ?>
+
+<script src="<?php echo $WEB_ROOT; ?>js/GameMaster.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/pokemon/Pokemon.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/PokeMultiSelect.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/Pokebox.js?=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/PokeSelect.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/PokeSearch.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/ModalWindow.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/interface/AttackChartInterface.js?v=<?php echo $SITE_VERSION; ?>"></script>
+<script src="<?php echo $WEB_ROOT; ?>js/Main.js?v=3"></script>
+
+<?php require_once 'footer.php'; ?>
---
+++ src/js/interface/AttackChartInterface.js
@@ -0,0 +1,327 @@
+// JavaScript Document
+
+let InterfaceMaster = (function () {
+    let instance;
+
+    function createInstance() {
+
+
+        let object = new interfaceObject();
+
+		function interfaceObject(){
+
+			let self = this;
+			let gm = GameMaster.getInstance();
+            let data = [];
+			let battle = new Battle();
+            let multiSelector = new PokeMultiSelect($(".custom-rankings-meta-group .poke.multi"));
+
+			this.init = function(){
+                multiSelector.init(gm.data.pokemon, battle);
+		        multiSelector.setContext("attackchart");
+
+                pokeSearch.setBattle(battle);
+
+				if(! get){
+                    battle.setCP(1500);
+                    battle.setCup("all");
+					this.loadRankings("1500","all");
+				} else{
+					this.loadGetData();
+				}
+
+				$(".format-select").on("change", selectFormat);
+				$("thead a").on("click", sortTable);
+				$("body").on("click", ".check", checkBox);
+
+				window.addEventListener('popstate', function(e) {
+					get = e.state;
+					self.loadGetData();
+				});
+			};
+
+			// Grabs ranking data from the Game Master
+
+			this.loadRankings = function(cp, cup){
+                gm.loadRankingData(self, "overall", cp, cup);
+			}
+
+			// Displays attack chart data for currently selected format
+
+			this.displayRankingData = function(){
+				// Load meta group
+                let metaKey = $(".format-select option:selected").attr("meta-group");
+
+                if(! gm.groups[metaKey]){
+                    runningResults = true;
+                    gm.loadGroupData(self, metaKey, gm.data);
+                    return false;
+                } else if(multiSelector.getPokemonList().length == 0){
+                    multiSelector.selectGroup(metaKey);
+                }
+
+                // Calculate results and display in table
+
+				$(".train-table tbody").html('');
+
+                // Filter Pokemon list by unique species
+                let pokemonList = [];
+                let multiselectorList = multiSelector.getPokemonList();
+
+                multiselectorList.forEach(pokemon => {
+                    if(! pokemonList.find(p => p.speciesId.replace("_shadow", "") == pokemon.speciesId.replace("_shadow", ""))){
+                        pokemonList.push(pokemon);
+                    }
+                })
+
+                // Collect table data
+                data = [];
+
+                pokemonList.forEach(pokemon => {
+                    let ivCombos = pokemon.generateIVCombinations("atk", 1, 4096);
+
+                    if(ivCombos.length == 0){
+                        console.error("No IV combinations found for " + pokemon.speciesId);
+                        return;
+                    }
+
+                    const minAtk = ivCombos[ivCombos.length - 1].atk;
+                    const maxAtk = ivCombos[0].atk;
+
+                    data.push(
+                        {
+                            pokemon: pokemon,
+                            minAtk: minAtk,
+                            maxAtk: maxAtk
+                        }
+                    );
+                });
+
+                // Sort data
+                data.sort((a,b) => (a.minAtk > b.minAtk) ? -1 : ((b.minAtk > a.minAtk) ? 1 : 0));
+
+                // Determine the x axis scale
+                const attackValues = data.flatMap(item => [item.minAtk, item.maxAtk]);
+                const chartMinAttack = Math.min.apply(null, attackValues) - 10;
+                const chartMaxAttack = Math.max.apply(null, attackValues) + 10;
+                const chartWidth = chartMaxAttack - chartMinAttack;
+
+                data.forEach(item => {
+                    const name = item.pokemon.speciesName.replace("(Shadow)", "");
+                    const displayMin = Math.floor(item.minAtk * 10) / 10;
+                    const displayMax = Math.floor(item.maxAtk * 10) / 10;
+                    const $row = $(".train-table tr.hide").clone().removeClass("hide");
+
+                    $row.find(".poke-name .name").html(name);
+                    $row.find(".min").html(displayMin);
+                    $row.find(".max").html(displayMax);
+                    $row.attr("data", item.pokemon.speciesId);
+
+                    const barWidth = ((item.maxAtk - item.minAtk) / chartWidth) * 100;
+                    $row.find(".cmp-item").width(barWidth + "%");
+
+                    const position = ((item.minAtk - chartMinAttack) / chartWidth) * 100;
+
+                    $row.find(".cmp-item").css("left", position + "%");
+
+                    const colorRating = (position / 100) * 1000;
+					const color = battle.getRatingColor(colorRating);
+					$row.find(".cmp-item").css("background-color", "rgb("+color[0]+","+color[1]+","+color[2]+")");
+
+                    $(".train-table tbody").append($row);
+                });
+			}
+
+			// Given JSON of get parameters, load these settings
+
+			this.loadGetData = function(){
+
+				if(! get){
+					return false;
+				}
+
+				// Cycle through parameters and set them
+				let cp = 1500;
+				let cup = "all";
+
+				for(let key in get){
+					if(get.hasOwnProperty(key)){
+
+						let val = get[key];
+
+						// Process each type of parameter
+
+						switch(key){
+
+							// Don't process default values so data doesn't needlessly reload
+
+							case "cp":
+								cp = val;
+								break;
+
+							case "cup":
+								cup = val;
+								break;
+						}
+					}
+				}
+
+				// Load data via existing change function
+
+				$(".format-select option[value=\""+cp+"\"][cup=\""+cup+"\"]").prop("selected","selected");
+
+				battle.setCP(cp);
+				battle.setCup(cup);
+				self.loadRankings(cp, cup);
+			}
+
+			// When the view state changes, push to browser history so it can be navigated forward or back
+
+			this.pushHistoryState = function(cup, cp){
+
+				let url = webRoot+"attack-cmp-chart/"+cup+"/"+cp+"/";
+
+				let data = {cup: cup, cp: cp };
+
+				window.history.pushState(data, "Attack Stat (CMP) Chart", url);
+			}
+
+
+			// Event handler for changing the format category
+
+			function selectFormat(e){
+				let cp = $(".format-select option:selected").val();
+				let cup = $(".format-select option:selected").attr("cup");
+
+				battle.setCP(cp);
+				battle.setCup(cup);
+
+                // Clear currently selected Pokemon
+                multiSelector.setPokemonList([]);
+                multiSelector.setCP(cp);
+
+				self.loadRankings(cp, cup);
+				self.pushHistoryState(cup, cp);
+			}
+
+			// Event handler for selecting ranking category
+
+			function sortTable(e){
+
+				e.preventDefault();
+
+				let $parent = $(e.target).closest("table");
+
+				$parent.find("thead a").removeClass("selected");
+
+				$(e.target).addClass("selected");
+
+				let targetData = data.performers;
+				let sortColumn = $(e.target).attr("data");
+
+				if($parent.hasClass("teams")){
+					targetData = data.teams;
+				}
+
+				switch(sortColumn){
+					case "name":
+						targetData.sort((a,b) => (a.pokemon > b.pokemon) ? 1 : ((b.pokemon > a.pokemon) ? -1 : 0));
+						break;
+
+					case "lead":
+						targetData.sort((a,b) => (a.team > b.team) ? 1 : ((b.team > a.team) ? -1 : 0));
+						break;
+
+					case "individual":
+						targetData.sort((a,b) => (a.individualScore > b.individualScore) ? -1 : ((b.individualScore > a.individualScore) ? 1 : 0));
+						break;
+
+					case "team":
+						targetData.sort((a,b) => (a.teamScore > b.teamScore) ? -1 : ((b.teamScore > a.teamScore) ? 1 : 0));
+						break;
+
+					case "usage":
+						targetData.sort((a,b) => (a.games > b.games) ? -1 : ((b.games > a.games) ? 1 : 0));
+						break;
+				}
+
+				self.displayRankingData(data);
+
+				submitSearchQuery();
+			}
+
+			let searchTimeout;
+			let searchStr = '';
+			let $target = null;
+
+			$("body").on("keyup", ".poke-search", function(e){
+				searchStr = $(this).val().toLowerCase().trim();
+
+				$target = $(".train-table."+$(e.target).attr("target"));
+
+				// Reset the timeout when a new key is typed. This prevents queries from being submitted too quickly and bogging things down on mobile.
+				window.clearTimeout(searchTimeout);
+				searchTimeout = window.setTimeout(submitSearchQuery, 200);
+			});
+
+			function submitSearchQuery(){
+				let list = GameMaster.getInstance().generatePokemonListFromSearchString(searchStr, battle);
+
+				if($target.hasClass("performers")){
+
+					// Search rows of top performers
+					$target.find("tbody tr").each(function(index, value){
+						let id = $(this).attr("data");
+
+						if(list.indexOf(id) > -1){
+							$(this).show();
+						} else{
+							$(this).hide();
+						}
+					});
+
+				} else if($target.hasClass("teams")){
+
+					// Search makeups of team
+					$target.find("tbody tr").each(function(index, value){
+						let $row = $(this);
+						let found = 0;
+
+						$row.find(".pokemon").each(function(spriteIndex, spriteValue){
+							let id = $(this).attr("data");
+
+							if(list.indexOf(id) > -1){
+								found++;
+							}
+						});
+
+						if(found >= 3 || (! searchStr.includes("!")) && found > 0){
+							$row.show();
+						} else{
+							$row.hide();
+						}
+
+					});
+				}
+			}
+
+			// Turn checkboxes on and off
+
+			function checkBox(e){
+				$(this).toggleClass("on");
+				$(this).trigger("stateChange");
+			}
+		};
+
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
