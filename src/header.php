bc7e5dd95a2eac50ba20beb73c80e6624c89fe6d
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-08-18 10:08:24 -0500

    Updated Eternatus moveset

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.1';
+$SITE_VERSION = '1.36.1.1';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
