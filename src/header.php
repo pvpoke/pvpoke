db8ba877650e2b1fcf51538356904f1b270b015e
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-09-13 20:59:47 -0500

    Ranking algorithm soft cap and low score curve
    Wins above 700 are significantly flattened for lesser impact. Losses below 300 are significantly curved for greater impact. This reduces the rankings of volatile Pokemon with hard wins/hard losses, and places more emphasizes on well rounded Pokemon with softer matchups.

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.3.10';
+$SITE_VERSION = '1.36.3.11';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
