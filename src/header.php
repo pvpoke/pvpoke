a5758b95479fe5b0ec0104f45725003e1767b09d
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-09-29 12:30:02 -0500

    Duraludon rankings

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.7.4';
+$SITE_VERSION = '1.36.7.5';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
