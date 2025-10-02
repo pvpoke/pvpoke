13d51e6081e0b83dd45465a0357394e9651eb477
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2025-10-02 11:12:44 -0500

    Updated meta groups
    Added Florges to Great League and P!P metas, updated Giratina moveset in Fantasy Cup

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.7.7';
+$SITE_VERSION = '1.36.7.8';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
