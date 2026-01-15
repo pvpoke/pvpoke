401f341da8d9be11a3fb76ab6f31eb40f3622da5
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2026-01-15 13:08:53 -0600

    Drum Beating stats, updated Glimmora moveset, updated BF GL

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.15.8';
+$SITE_VERSION = '1.36.15.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
