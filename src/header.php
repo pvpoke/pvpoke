eae8765661a2e38f47495a13be9b56467b3842e8
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2026-01-29 11:07:16 -0600

    Updated P!P meta group and Great League editor notes

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.15.15';
+$SITE_VERSION = '1.36.15.16';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
