81c5f09d0209f2c23d91821c42c818111c365d81
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2026-02-05 11:01:35 -0600

    Updated Enamorus Therian moveset

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.16.3';
+$SITE_VERSION = '1.36.16.6';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
