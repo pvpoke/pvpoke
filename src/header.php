4e3dea49c5794fe0a9bddc5bf6d1bb7b9c780fd6
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2026-02-06 09:15:25 -0600

    Updated EUIC meta group

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.36.16.6';
+$SITE_VERSION = '1.36.16.7';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
