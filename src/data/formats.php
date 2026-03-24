57b4e6d553f72b413ab091abd3ab8cd14d5dec0b
Empoleon Dynamite <pvpoke.empoleon@gmail.com>
2026-03-23 19:37:02 -0500

    Electric Cup rankings, fixed BF movesets

--- src/data/formats.php
+++ src/data/formats.php
@@ -30,22 +30,6 @@ $formats = array (
   ),
   2 => 
   array (
-    'title' => 'Spring Cup',
-    'cup' => 'spring',
-    'cp' => 1500,
-    'meta' => 'spring',
-    'showCup' => true,
-    'showFormat' => true,
-    'showMeta' => true,
-    'rules' => 
-    array (
-      0 => 'Pokémon must be at or below 1,500 CP to enter.',
-      1 => 'Only Water-, Grass-, and Fairy-type Pokémon are eligible.',
-      2 => 'The following Pokemon are not allowed: Jumpluff, Roserade, Toxapex',
-    ),
-  ),
-  3 => 
-  array (
     'title' => 'Jungle Cup',
     'cup' => 'jungle',
     'cp' => 1500,
@@ -57,6 +41,22 @@ $formats = array (
     array (
       0 => 'Pokémon must be at or below 1,500 CP to enter.',
       1 => 'Only Normal-, Grass-, Electric-, Poison-, Ground-, Flying-, Bug-, and Dark-type Pokémon are eligible.',
+    ),
+  ),
+  3 => 
+  array (
+    'title' => 'Electric Cup',
+    'cup' => 'electric',
+    'cp' => 1500,
+    'meta' => 'electric',
+    'showCup' => true,
+    'showFormat' => true,
+    'showMeta' => true,
+    'rules' => 
+    array (
+      0 => 'Pokémon must be at or below 1,500 CP to enter.',
+      1 => 'Only Electric-type Pokémon are eligible.',
+      2 => 'The following Pokemon are not allowed: Stunfisk, Heliolisk, Charjabug, Vikavolt',
     ),
   ),
   4 => 
