e8ae1a65c67ae0613d812cadd66b6eca501ef718
Gerardo Treviño <geratrevino115@gmail.com>
2026-09-02 12:27:41 -0600

    Keep localization scoped to move names

--- src/header.php
+++ src/header.php
@@ -92,7 +92,7 @@ if(! isset($_COOKIE['migrate'])){
 
 ?>
 <!doctype html>
-<html lang="<?php echo $_SETTINGS->language; ?>">
+<html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
