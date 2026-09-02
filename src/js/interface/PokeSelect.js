82a974ffa6e103c125a4a9e2012d21836861ef96
Empoleon_Dynamite <46719444+pvpoke@users.noreply.github.com>
2026-09-02 09:02:53 -0500

    Mega Level 4 CP Boost
    Implemented 2 level boost for Mega Level 4, updated Mega ML rankings.

--- src/js/interface/PokeSelect.js
+++ src/js/interface/PokeSelect.js
@@ -342,11 +342,17 @@ function PokeSelect(element, i){
 				baseId = baseId.replace("_primal", "");
 
 				var basePokemon = new Pokemon(baseId, index, battle);
+				var basePokemonLevel = selectedPokemon.level;
+
+				if(selectedPokemon.megaLevel == 4){
+					basePokemonLevel -= 2;
+				}
+
 				basePokemon.initialize(false);
 				basePokemon.setIV("atk", selectedPokemon.ivs.atk);
 				basePokemon.setIV("def", selectedPokemon.ivs.def);
 				basePokemon.setIV("hp", selectedPokemon.ivs.hp);
-				basePokemon.setLevel(selectedPokemon.level);
+				basePokemon.setLevel(basePokemonLevel);
 
 				$el.find(".mega-cp-container .base-name").html("Base " + basePokemon.speciesName);
 				$el.find(".mega-cp-container .mega-cp .stat").html(basePokemon.cp);
@@ -1181,9 +1187,9 @@ function PokeSelect(element, i){
 		// Select mega or shadow form of top result
 		if(typeof idSuffix == "string" && idToSelect){
 			idToSelect = idToSelect.replace('_shadow', '');
-			idToSelect = idToSelect.replace('_mega', '');
 			idToSelect = idToSelect.replace('_mega_x', '');
 			idToSelect = idToSelect.replace('_mega_y', '');
+			idToSelect = idToSelect.replace('_mega', '');
 			idToSelect = idToSelect.replace('_primal', '');
 			idToSelect = idToSelect.replace('_alolan', '');
 			idToSelect = idToSelect.replace('_galarian', '');
@@ -1303,7 +1309,10 @@ function PokeSelect(element, i){
 		var sortStat = $el.find(".maximize-section .check-group .check.on").first().attr("value");
 		var levelCap = parseInt($el.find(".maximize-section .level-cap-group .check.on").first().attr("value"));
 
-		selectedPokemon.levelCap = levelCap;
+		if(! isNaN(levelCap)){
+			selectedPokemon.levelCap = levelCap;
+		}
+		
         selectedPokemon.maximizeStat(sortStat);
 
         selectedPokemon.isCustom = true;
